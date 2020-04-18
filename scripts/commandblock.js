4ee3939f4b5648b81c7fe4e71326d778b31e00ff
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-18 20:59:06 +0900

    DETACH COMMAND

--- scripts/commandblock.js
+++ scripts/commandblock.js
@@ -1,32 +1,5 @@
+import {command} from "command.js";
 const commandblock = extendContent(MessageBlock, "commandblock", {
-  command(tile,msg){
-    if(msg.substring(0,1)!="/") msg="/"+msg;
-    var argstmp = msg.substring(1).split('"');
-    var args=[];
-    for(var i=0;i<argstmp.length;i++){
-      if(i%2==0){
-        if(argstmp[i].trim()!=''){
-          args=args.concat(argstmp[i].trim().split(' '));
-        }
-      }
-      else{
-        args.push(argstmp[i].trim());
-      }
-    }
-    if(args.length==0){
-      return;
-    }
-    var cmd = args[0];
-    args = args.splice(1);
-    switch(cmd){
-      case 'overwrite':
-        this.setMessageBlockText(null,tile,args.join(' '));
-      break;
-      case 'say':
-        Call.sendMessage(args.join(' '));
-      break;
-    }
-  },
   init(){
     this.super$init();
 		this.didcmd = false;
