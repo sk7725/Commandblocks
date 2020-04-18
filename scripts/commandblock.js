2ce9d800aa36c6c637dc0e426645a239c418205d
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-19 08:46:00 +0900

    Update commandblock.js

--- scripts/commandblock.js
+++ scripts/commandblock.js
@@ -1,36 +1,5 @@
 
 const commandblock = extendContent(MessageBlock, "commandblock", {
-  command(tile,msg){
-    if(msg.substring(0,1)!="/") msg="/"+msg;
-    var argstmp = msg.substring(1).split('"');
-    var args=[];
-    for(var i=0;i<argstmp.length;i++){
-      if(i%2==0){
-        if(argstmp[i].trim()!=''){
-          args=args.concat(argstmp[i].trim().split(' '));
-        }
-      }
-      else{
-        args.push(argstmp[i].trim());
-      }
-    }
-    if(args.length==0){
-      return;
-    }
-    var cmd = args[0];
-    args = args.splice(1);
-    switch(cmd){
-      case 'overwrite':
-        this.setMessageBlockText(null,tile,args.join(' '));
-      break;
-      case 'say':
-        Call.sendMessage(args.join(' '));
-      break;
-      case 'title':
-        Vars.ui.scriptfrag.addMessage(args.join(' '));
-      break;
-    }
-  },
   init(){
     this.super$init();
 		this.didcmd = false;
@@ -41,7 +10,7 @@ const commandblock = extendContent(MessageBlock, "commandblock", {
       this.super$update(tile);
       //entity.cons.trigger();
       if(!this.didcmd){
-        this.command(tile,entity.message);
+        this.global.commandblocks.command(tile,entity.message);
         this.didcmd = true;
       }
 
