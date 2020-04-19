fd3f07492e387e4b0225a1297e545a501f10ecea
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-19 10:11:45 +0900

    Create commandblockchained.js

---
+++ scripts/commandblockchained.js
@@ -0,0 +1,33 @@
+const commandblocks=this.global.commandblocks;
+const commandblockchained = extendContent(MessageBlock, "commandblockchained", { 
+getfacingpos(tx, ty, trot){
+var tmpobj={};
+if(trot==0){
+tmpobj.x=tx+1;  tmpobj.y=ty;
+}
+else if(trot==1){
+tmpobj.x=tx;  tmpobj.y=ty+1;
+}
+else if(trot==2){
+tmpobj.x=tx-1;  tmpobj.y=ty;
+}
+else{
+tmpobj.x=tx;  
+tmpobj.y=ty-1;
+}
+return tmpobj;
+},		
+update(tile){ 
+var entity=tile.ent(); 
+if(tile.entity.cons.valid()){ 
+this.super$update(tile); //entity.cons.trigger(); 
+const facepos=this.getfacingpos(tile.x,tile.y,tile.rotation());		
+const x=facepos.x; const y=facepos.y;		
+var near = Vars.world.tile(x,y);
+this.success=commandblocks.command(tile,entity.message); 
+} 
+else{ 
+this.success=false; return; 
+} 
+}
+});
