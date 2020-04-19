0c5eaa2b28844a79f0e974177a9b8fef6673f8c3
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-19 22:53:00 +0900

    Update command.js

--- scripts/command.js
+++ scripts/command.js
@@ -292,7 +292,7 @@ const commandblocks={
                 ctile.block().onDestroyed(ctile);
               }
               else{
-                ctile.entity.onDeath();
+                ctile.ent().remove();
               }
               Vars.world.tile(cx, cy).removeNet();
               if(args[5]=="build"){
