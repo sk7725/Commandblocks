32f07d2427372612ae892b353bf33ae2ff422e7e
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-19 13:46:14 +0900

    listing!!!

--- scripts/commandblockchained.js
+++ scripts/commandblockchained.js
@@ -1,45 +1,4 @@
 const commandblocksc=this.global.commandblocks;
-/*
-const commandblockchained = extendContent(MessageBlock, "commandblockchained", { 
-getfacingpos(tx, ty, trot){
-var tmpobj={};
-if(trot==0){
-tmpobj.x=tx-1;  tmpobj.y=ty;
-}
-else if(trot==1){
-tmpobj.x=tx;  tmpobj.y=ty-1;
-}
-else if(trot==2){
-tmpobj.x=tx+1;  tmpobj.y=ty;
-}
-else{
-tmpobj.x=tx;  tmpobj.y=ty+1;
-}
-return tmpobj;
-},		
-update(tile){ 
-var entity=tile.ent(); 
-if(tile.entity.cons.valid()){ 
-this.super$update(tile); //entity.cons.trigger(); 
-const facepos=this.getfacingpos(tile.x,tile.y,tile.rotation());		
-const x=facepos.x; const y=facepos.y;		
-var near = Vars.world.tile(x,y);
-if(near.block()=="commandblock"||near.block()=="commandblockrepeating"||near.block()="commandblockchained"){
-if(true){
-this.success=commandblocksc.command(tile,entity.message); 
-}
-else{
-this.success=false;
-}
-}
-} 
-else{ 
-this.success=false; return; 
-} 
-}
-});
-*/
-
 const commandblockchained = extendContent(MessageBlock, "commandblockchained", {
   getawaypos(tx, ty, trot){
 var tmpobj={};
@@ -59,6 +18,26 @@ return tmpobj;
   },		
   update(tile){
     var entity=tile.ent();
+    var key=tile.x+","+tile.y;
+    if(!commandblocksc.hasOwnProperty(key)) commandblocksc[key]={};
+    thiscmd=commandblocksc[key];
+    if(tile.entity.cons.valid()){
+      this.super$update(tile);
+      const awaypos=this.getawaypos(tile.x,tile.y,tile.rotation());	
+      const akey=awaypos.x+","+awaypos.y;
+      if(commandblocksc.hasOwnProperty(akey)&&commandblocksc[akey].didsuccess){
+        thiscmd.didsuccess = commandblocksc.command(tile,entity.message,this);
+      }
+      else{
+      thiscmd.didsuccess=false;
+      return;
+      }
+    }
+    else{
+      thiscmd.didsuccess=false;
+      return;
+    }
+/*
     if(tile.entity.cons.valid()){
       this.super$update(tile);
       //entity.cons.trigger();
@@ -73,6 +52,7 @@ return tmpobj;
       tile.didsuccess=false;
       return;
     }
+*/
   }
 });
 
