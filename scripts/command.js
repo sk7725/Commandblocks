c8382bb9defe2168faebc7ab3debf9b5ff2e7767
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-19 14:57:57 +0900

    setblock

--- scripts/command.js
+++ scripts/command.js
@@ -32,12 +32,39 @@ const commandblocks={
         Vars.ui.scriptfrag.addMessage(args.join(' '));
         return false;
       break;
+      case 'setblock':
+        //Call.setTile(Vars.world.tile(tile.x, tile.y), Blocks.air, tile.team, rot);
+        if(args.length>=3&&args.length<=5){
+          var cx=args[0]; var cy=args[1]; var cblock=args[2];
+          if(cx>=0&&cy>=0){
+            if(Blocks.hasOwnProperty(cblock)){
+              if(args.length==3) Call.setTile(Vars.world.tile(cx, cy), Blocks[cblock], tile.team, 0);
+              if(args.length==4){ 
+                if(args[3]>=0&&args[3]<=3) Call.setTile(Vars.world.tile(cx, cy), Blocks[cblock], tile.team, args[3]);
+                else throw "Rotation should be 0~3";
+              }
+              if(args.length==5){ 
+                if(args[3]>=0&&args[3]<=3) Call.setTile(Vars.world.tile(cx, cy), Blocks[cblock], args[4], args[3]);
+                else throw "Rotation should be 0~3";
+              }
+            }
+            else throw "No such Block or Entity";
+          }
+          else{
+            throw "Coordinates should be above 0";
+          }
+        }
+        else{
+          Call.sendMessage("E:Missing params.");
+          return false;
+        }
+      break;
       default:
         return false;
     }
   }
   catch(err){
-    Call.sendMessage(err);
+    Call.sendMessage("E:"+err);
     return false;
   }
   }
