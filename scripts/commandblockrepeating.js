642295c5b6951addf911a7e229b5fbcb7ea99711
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-19 12:05:42 +0900

    Update commandblockrepeating.js

--- scripts/commandblockrepeating.js
+++ scripts/commandblockrepeating.js
@@ -1,11 +1,4 @@
-/*
-MessageBlockEntity entity = tile.ent();
-if(entity != null){
-    entity.message = result.toString();
-    entity.lines = entity.message.split("\n");
-}
-*/
-//cmd=require("commandblock");
+
 const commandblocksr=this.global.commandblocks;
 const commandblockrepeating = extendContent(MessageBlock, "commandblockrepeating", {
   update(tile){
@@ -20,43 +13,4 @@ const commandblockrepeating = extendContent(MessageBlock, "commandblockrepeating
       return;
     }
   }
-  /*
-	draw(tile) {
-		Draw.rect(Core.atlas.find(this.name + "_" + tile.x % 2),
-			tile.drawx(),
-			tile.drawy());
-	},
-
-	generateIcons() {
-		return [Core.atlas.find(this.name)];
-	},
-
-	calcOffset(tile) {
-		var x = tile.x;
-		if (x % 2 == 0) {
-			x++;
-		} else {
-			x--;
-		}
-		return x;
-	},
-
-	canPlaceOn(tile){
-		const x = this.calcOffset(tile);
-		const other = Vars.world.tile(x, tile.y);
-		return other.block() == "air"
-	},
-  */
-/*
-	removed(tile) {
-		this.super$removed(tile);
-		const x = this.calcOffset(tile);
-		const key = tile.x + "," + tile.y;
-		//Prevent trying to delete the other half infinitely
-		if (alive[key]) {
-			alive[key] = false;
-			Call.setTile(Vars.world.tile(x, tile.y), Blocks.air, tile.team, 0);
-		}
-	},*/
-	//chad: false
 });
