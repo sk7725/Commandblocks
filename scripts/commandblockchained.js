3a2831fc5c1cdf1a6b0fc849d1cc1d9695280f6d
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-19 12:36:19 +0900

    getawaypos

--- scripts/commandblockchained.js
+++ scripts/commandblockchained.js
@@ -41,12 +41,28 @@ this.success=false; return;
 */
 
 const commandblockchained = extendContent(MessageBlock, "commandblockchained", {
+  getawaypos(tx, ty, trot){
+var tmpobj={};
+if(trot==0){
+tmpobj.x=tx-1;  tmpobj.y=ty;
+}
+else if(trot==1){
+tmpobj.x=tx;  tmpobj.y=ty-1;
+}
+else if(trot==2){
+tmpobj.x=tx+1;  tmpobj.y=ty;
+}
+else{
+tmpobj.x=tx;  tmpobj.y=ty+1;
+}
+return tmpobj;
+  },		
   update(tile){
     var entity=tile.ent();
     if(tile.entity.cons.valid()){
       this.super$update(tile);
       //entity.cons.trigger();
-      this.success=commandblocksc.command(tile,entity.message);
+      this.success=commandblocksc.command(tile,entity.message,this);
     }
     else{
       this.success=false;
