8ff525ec39d9bc87266e3f891c9464c3ac0678ad
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-19 13:25:08 +0900

    tracking...

--- scripts/commandblock.js
+++ scripts/commandblock.js
@@ -6,19 +6,22 @@ const commandblock = extendContent(MessageBlock, "commandblock", {
 	},
   update(tile){
     var entity=tile.ent();
-    if(!tile.hasOwnProperty('didcmd') tile.didcmd=false;
+    var key=tile.x+","+tile.y;
+    if(!this.global.commandblocks.hasOwnProperty(key)) this.global.commandblocks[key]={};
+    thiscmd=this.global.commandblocks[key];
+    if(!thiscmd.hasOwnProperty("didcmd") thiscmd.didcmd=false;
     if(tile.entity.cons.valid()){
       this.super$update(tile);
       //entity.cons.trigger();
-      if(!tile.didcmd){
-        tile.didsuccess = commandblocksi.command(tile,entity.message,this);
-        tile.didcmd = true;
+      if(!thiscmd.didcmd){
+        thiscmd.didsuccess = commandblocksi.command(tile,entity.message,this);
+        thiscmd.didcmd = true;
       }
 
     }
     else{
-      if(tile.didcmd) tile.didcmd=false;
-      tile.didsuccess=false;
+      if(thiscmd.didcmd) thiscmd.didcmd=false;
+      thiscmd.didsuccess=false;
       return;
     }
   }
