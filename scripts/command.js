1e550faaff0353826f8635f65240393a8a55a797
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-23 08:51:07 +0900

    Update command.js

--- scripts/command.js
+++ scripts/command.js
@@ -2,6 +2,8 @@ var gamerule={};
 gamerule.commandBlockOutput=true;
 gamerule.commandBlockTitle=false;
 
+var seff= extendContent(StatusEffect,"seff",{});
+
 if(!this.global.hasOwnProperty("commandcached")) this.global.commandcached={};
 const commandcached=this.global.commandcached;
 const commandblocks={
@@ -252,7 +254,7 @@ const commandblocks={
     print("C:"+err);
   },
   cmdeffect(punit,eff,duration,intensity,hidep){
-    var seff= extendContent(StatusEffect,"seff",{});
+    
     var seffcolor= Color.valueOf("ffffff");
     switch(eff.trim()){
       case "speed":
