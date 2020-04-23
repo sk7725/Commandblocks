63ec6f3236fd2743e4dc4bc60f0af5c087b85222
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-24 08:43:07 +0900

    Update playermusic.js

--- scripts/playermusic.js
+++ scripts/playermusic.js
@@ -10,7 +10,8 @@ const playermusic = extendContent(MessageBlock, "playermusic", {
 			//var muse=Vars.content.getByName(ContentType.unit,"commandblocks-xmusic-"+musename).activeSound;
 			//Vars.control.music.play(muse);
 			//if music=sound(unlikely)
-                        var thismod = Vars.mods.locateMod("commandblocks").root; var musefi=thismod.child(!Vars.ios ? "music-"+musename + ".ogg" :"music-"+ musename + ".mp3");
+                        var thismod = Vars.mods.locateMod("commandblocks").file; //var musefi=thismod.child(!Vars.ios ? "music-"+musename + ".ogg" :"music-"+ musename + ".mp3");
+                        var musefi=thismod.child("mod.json");
 			if(musefi.exists()){
 				//var mpath = Vars.tree.resolve("music-"+musename + ".ogg").exists() && !Vars.ios ? "music-"+musename + ".ogg" :"music-"+ musename + ".mp3";
                                 if(muselist.hasOwnProperty(musename)) return; //already playing
