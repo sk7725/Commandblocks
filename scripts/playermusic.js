f1046ad0562afe41851c00e9dba515662bc7b950
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-23 15:24:18 +0900

    Update playermusic.js

--- scripts/playermusic.js
+++ scripts/playermusic.js
@@ -1,45 +1,5 @@
-/*
-MessageBlockEntity entity = tile.ent();
-if(entity != null){
-    entity.message = result.toString();
-    entity.lines = entity.message.split("\n");
-}
-*/
+
 const playermusic = extendContent(MessageBlock, "playermusic", {
-  /*
-	draw(tile) {
-		Draw.rect(Core.atlas.find(this.name + "_" + tile.x % 2),
-			tile.drawx(),
-			tile.drawy());
-	},
-
-	generateIcons() {
-		return [Core.atlas.find(this.name)];
-	},
-
-	calcOffset(tile) {
-		var x = tile.x;
-		if (x % 2 == 0) {
-			x++;
-		} else {
-			x--;
-		}
-		return x;
-	},
-
-	canPlaceOn(tile){
-		const x = this.calcOffset(tile);
-		const other = Vars.world.tile(x, tile.y);
-		return other.block() == "air"
-	},
-  */
-	buildConfiguration(tile, table){
-		table.addImageButton(
-			Icon.arrowRightSmall,
-			Styles.clearTransi,
-			run(() => tile.configure(0))
-		).size(40);
-	},
 	configured(tile,value){
 		var entity=tile.ent();
 		var musename=entity.message;
