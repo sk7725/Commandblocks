4322574cfb8b8e7f38b0473a18e6e411dd4c28bd
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-23 16:20:00 +0900

    muse...............

--- scripts/playermusic.js
+++ scripts/playermusic.js
@@ -1,10 +1,9 @@
 
 const playermusic = extendContent(MessageBlock, "playermusic", {
-	configured(tile,value){
-		var entity=tile.ent();
-		var musename=entity.message;
+	playmuse(musename){
 		if(!musename){
 			Vars.ui.showInfoToast("Played nothing!",7);
+			Vars.control.music.play(null);
 		}
 		else{
 			//var muse=Vars.content.getByName(ContentType.unit,"commandblocks-xmusic-"+musename).activeSound;
@@ -13,12 +12,27 @@ const playermusic = extendContent(MessageBlock, "playermusic", {
 			if(Vars.tree.get("music-"+musename + ".ogg").exists()){
 				var mpath = Vars.tree.get("music-"+musename + ".ogg").exists() && !Vars.ios ? "music-"+musename + ".ogg" :"music-"+ musename + ".mp3";
 				var muse = newMusic(mpath);
-				Vars.ui.showInfoToast("Playing music-"+musename,7);
+				Vars.ui.showInfoToast("Playing music-"+musename,7+"...");
 				Vars.control.music.play(muse);
 			}
 			else{
-				Vars.ui.showInfoToast("Cannot find music-"+musename,7);
+				Vars.ui.showInfoToast("Cannot find music-"+musename,7+"!");
 			}
+		}
+	},
+	update(tile){
+		var entity=tile.ent();
+		var musename=entity.message;
+		if(tile.entity.cons.valid()){
+      this.super$update(tile);
+      //entity.cons.trigger();
+      if(!entity.hasOwnProperty("played")||!entity.played){
+        entity.played=true;
+				this.playmuse(musename);
+      }
+    }
+		else{
+			entity.played=false;
 		}
 	}
 });
