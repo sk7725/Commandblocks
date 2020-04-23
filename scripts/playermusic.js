8581dc7d4f64f7f279550e22394c28b3297d7634
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-23 15:37:33 +0900

    Update playermusic.js

--- scripts/playermusic.js
+++ scripts/playermusic.js
@@ -3,17 +3,22 @@ const playermusic = extendContent(MessageBlock, "playermusic", {
 	configured(tile,value){
 		var entity=tile.ent();
 		var musename=entity.message;
-		if(musename==""){
+		if(!musename){
 			Vars.control.music.play(null);
+			Vars.ui.showInfoToast("Played nothing!",7);
 		}
 		else{
 			//var muse=Vars.content.getByName(ContentType.unit,"commandblocks-xmusic-"+musename).activeSound;
 			//Vars.control.music.play(muse);
 			//if music=sound(unlikely)
 			if(Vars.tree.get("music-"+musename + ".ogg").exists()){
-				var mpath = Vars.tree.get("music-"+musename + ".ogg").exists() && !Vars.ios ? musename + ".ogg" : musename + ".mp3";
+				var mpath = Vars.tree.get("music-"+musename + ".ogg").exists() && !Vars.ios ? "music-"+musename + ".ogg" :"music-"+ musename + ".mp3";
 				var muse = newMusic(mpath);
+				Vars.ui.showInfoToast("Playing music-"+musename,7);
 				Vars.control.music.play(muse);
+			}
+			else{
+				Vars.ui.showInfoToast("Cannot find music-"+musename,7);
 			}
 		}
 	}
