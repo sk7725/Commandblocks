5747355ec5f5764861cd5efd331247d2a83ced38
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-25 17:52:02 +0900

    pwr

--- scripts/button.js
+++ scripts/button.js
@@ -7,14 +7,16 @@ const button = extendContent(Block, "button", {
   draw(tile) {
     Draw.rect(Core.atlas.find(this.name + ((tile.ent().timer.check(timerid,presstick)) ? "":"-trig")), tile.drawx(), tile.drawy()); 
   }, 	
-  generateIcons() {
-    return [Core.atlas.find(this.name)];
-  },
   tapped(tile,player){
     tile.ent().timer.reset(timerid,0);
-    Sounds.click.at(tile.worldx(),tile.worldy(),0.4);
+    Sounds.click.at(tile.worldx(),tile.worldy());
   },
+/*
   update(tile){
     if(tile.ent().timer.getTime(timerid)==presstick) Sounds.click.at(tile.worldx(),tile.worldy(),0.8);
+  }
+*/
+  getPowerProduction(tile){
+  (tile.ent().timer.check(timerid,presstick)) ? 0: 180;
   }
 });
