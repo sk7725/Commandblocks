ff7c6951fb17e0ea8b8135e75c792c46c903b91b
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-25 23:21:01 +0900

    Create buttonrouter.js

---
+++ scripts/buttonrouter.js
@@ -0,0 +1,18 @@
+const presstick=6; const timerid=0;
+const buttonrouter = extendContent(Router, "buttonrouter", {
+  placed(tile) {
+    this.super$placed(tile);
+    tile.ent().timer.reset(timerid,presstick+1);
+  },
+  draw(tile) {
+    Draw.rect(Core.atlas.find(this.name + ((tile.ent().timer.check(timerid,presstick)) ? "":"-trig")), tile.drawx(), tile.drawy());
+  },
+  update(tile){
+    this.super$update(tile);
+    var entity=tile.ent();
+    if(tile.entity.items.total()>0) tile.ent().timer.reset(timerid,0);
+  },
+  getPowerProduction(tile){
+  return (tile.ent().timer.check(timerid,presstick)) ? 0: 1;
+  }
+});
