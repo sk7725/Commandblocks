7f32c727791e1f2485690d1241e6bc3792b69049
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-25 15:11:59 +0900

    Create button.js

---
+++ scripts/button.js
@@ -0,0 +1,16 @@
+const presstick=20; const timerid=0;
+const button = extendContent(Block, "button", {	
+  placed(tile) {
+    this.super$placed(tile);
+    tile.ent().timer.reset(timerid,presstick);
+  },
+  draw(tile) {
+    Draw.rect(Core.atlas.find(this.name + ((tile.ent().timer.check(timerid,presstick)) ? "":"-trig")), tile.drawx(), tile.drawy()); 
+  }, 	
+  generateIcons() {
+    return [Core.atlas.find(this.name)];
+  },
+  tapped(tile,player){
+  
+  }
+};
