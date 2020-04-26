19d326128fd34767acca50e01a261399a1a252b3
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-26 11:40:23 +0900

    Update camwall.js

--- scripts/camwall.js
+++ scripts/camwall.js
@@ -1,18 +1,18 @@
 const placetick=80; const showtick=60; const showdelay=300; const placeid=0; const showid=1;
-var nearplaced=false;
+var nearplaced=-1*(showtick+showdelay);
 const camwall = extendContent(Block, "camwall", {
   placed(tile) {
     this.super$placed(tile);
     tile.ent().timer.reset(placeid,0);
     tile.ent().timer.reset(showid,1);
-    nearplaced=true;
+    nearplaced=Time.time();
   },
   draw(tile) {
     if(nearplaced&&tile.ent().timer.getTime(showid)==0) nearplaced=false;
     if(nearplaced) tile.ent().timer.reset(showid,0);
     Draw.color(Color.rgb(255,255,255).a(Math.max(0,placetick-tile.ent().timer.getTime(placeid))/placetick));
     Draw.rect(Core.atlas.find(this.name), tile.drawx(), tile.drawy());
-    Draw.color(Color.rgb(255,255,255).a(Math.max(0,Math.min(showtick,showdelay+showtick-tile.ent().timer.getTime(showid)))/showtick));
+    Draw.color(Color.rgb(255,255,255).a(Math.max(0,Math.min(showtick,showdelay+showtick-(Time.time()-nearplaced)))/showtick));
     Draw.rect(Core.atlas.find(this.name+"-top"), tile.drawx(), tile.drawy());
     Draw.color();
   }
