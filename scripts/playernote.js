f01ad838c76fc6f0b72290ec8d0cbc52d69b635b
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-26 19:17:57 +0900

    Update playernote.js

--- scripts/playernote.js
+++ scripts/playernote.js
@@ -63,7 +63,7 @@ const playernote = extendContent(MessageBlock, "playernote", {
     try{
       var soundblock=Vars.content.getByName(ContentType.block,soundlib+"-"+inst+"-"+(Math.floor((notein+9)/12)+4)+""+notes[notein %12]);
       if(instalt) soundblock.idleSound.play(600);
-      else soundblock.activeSound.play(600);
+      while(!soundblock.activeSound.play(600)){}
     }
     catch(err){
       print(err);
