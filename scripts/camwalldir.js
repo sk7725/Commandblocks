93e061e38195811abe941394822aa12ef4c23a5a
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-26 14:56:08 +0900

    eh global

--- scripts/camwalldir.js
+++ scripts/camwalldir.js
@@ -1,16 +1,18 @@
 const placetick=80; const showtick=60; const showdelay=300; const placeid=0; const showid=1;
 this.global.camwall=-1*(showtick+showdelay);
 nearplaced=this.global.camwall;
+const t=this;
 const camwalldir = extendContent(Block, "camwalldir", {
   placed(tile) {
     this.super$placed(tile);
     tile.ent().timer.reset(placeid,0);
     tile.ent().timer.reset(showid,1);
-    nearplaced=Time.time();
+    t.global.camwall=Time.time();
   },
   draw(tile) {
     //if(nearplaced&&tile.ent().timer.getTime(showid)==0) nearplaced=false;
     //if(nearplaced) tile.ent().timer.reset(showid,0);
+    nearplaced=t.global.camwall;
     Draw.color(Color.rgb(255,255,255).a(Math.max(0,placetick-tile.ent().timer.getTime(placeid))/placetick));
     Draw.rect(Core.atlas.find(this.name+"-base"), tile.drawx(), tile.drawy());
     Draw.color(Color.rgb(255,255,255).a(Math.max(0,Math.min(showtick,showdelay+showtick-(Time.time()-nearplaced)))/showtick));
