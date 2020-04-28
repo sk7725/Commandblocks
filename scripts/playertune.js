de7248c57b58b215464c77b30f06052a85ec42a0
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-28 15:25:11 +0900

    Update playertune.js

--- scripts/playertune.js
+++ scripts/playertune.js
@@ -90,7 +90,7 @@ const playertune = extendContent(MessageBlock, "playertune", {
   }
 });
 
-playertune.entityType=prov(()=>extendContent(MessageBlock.MessageBlockEntity,{
+playertune.entityType=prov(()=>extend(MessageBlock.MessageBlockEntity,{
   config(){
     return this.message;
   }
