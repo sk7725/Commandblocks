52380e02eaa681165c3224c46c23e2069d70a8ad
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-04-29 12:24:31 +0900

    Update setconv.js

--- scripts/setconv.js
+++ scripts/setconv.js
@@ -3,8 +3,8 @@ t.global.setconvspeed=0.2;
 const setconv = extendContent(Conveyor, "setconv", {
 
 	update(tile){
-		if(this.speed!=t.global.setconvspeed*0.1){
-			this.speed=t.global.setconvspeed*0.1;
+		if(this.speed!=t.global.setconvspeed){
+			this.speed=t.global.setconvspeed;
 		}
 		this.super$update(tile);
 	}
