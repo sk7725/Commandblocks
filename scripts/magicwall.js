19a68901b970b31e34f68a7b804955f33dbf9c54
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-02 22:40:27 +0900

    Update magicwall.js

--- scripts/magicwall.js
+++ scripts/magicwall.js
@@ -12,18 +12,19 @@ const magicwall=extendContent(Wall,"magicwall",{
       this.super$load();
       this.region=Core.atlas.find(this.name);
       this.animRegion=Core.atlas.find(this.name+"-anim");
+      this.replaceWalls=["copperWall", "titaniumWall", "plastaniumWall", "thoriumWall","phaseWall", "surgeWall"];
+      this.thisBlock=Vars.content.getByName(ContentType.block,this.name);
     },
     init(){
       this.super$init();
 
-      this.thisBlock=Vars.content.getByName(ContentType.block,this.name);
+
     },
     onDestroyed(tile){
       this.super$onDestroyed(tile);
       var replacement=this.thisBlock;
       if(Math.random()>0.5){
-        this.replaceWalls=Vars.content.blocks().copy().eachFilter(boolf(e=>(!(e instanceof Wall)))).toArray();
-        replacement=this.replaceWalls[Math.floor(Math.random() * this.replaceWalls.length)];
+        replacement=Blocks[this.replaceWalls[Math.floor(Math.random() * this.replaceWalls.length)]];
       }
       Vars.world.tile(tile.x, tile.y).setNet(replacement, tile.team, 0);
     }
