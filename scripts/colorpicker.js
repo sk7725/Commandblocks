1295ed4caae93117d01fe350b2e5efea78bbe71c
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-03 22:42:59 +0900

    if this works i criiiii

--- scripts/colorpicker.js
+++ scripts/colorpicker.js
@@ -5,6 +5,8 @@ const colorpicker = extendContent(Block, "colorpicker", {
   playerPlaced(tile){
     tile.configure(t.global.colors.brushcolor); 
   },
+  load(){
+  },
   draw(tile){ 
   //super.draw(tile); LightEntity entity = tile.ent(); Draw.blend(Blending.additive); Draw.color(Tmp.c1.set(entity.color), entity.efficiency() * 0.3f); Draw.rect(reg(topRegion), tile.drawx(), tile.drawy()); Draw.color(); Draw.blend(); 
   //Tmp.c1.set(tile.ent().color)
@@ -13,7 +15,12 @@ const colorpicker = extendContent(Block, "colorpicker", {
   buildConfiguration(tile, table){
     //var entity = tile.ent(); 
     //js-fy this at home
-    table.addImageButton(Icon.pencil, () => { Vars.ui.picker.show(Tmp.c1.set(t.global.colors.brushcolor), false, res => { t.global.colors.brushcolor = res.rgba(); }); Vars.control.input.frag.config.hideConfig(); }).size(40); 
+    table.addImageButton(Icon.pencil, run(() => { 
+      Vars.ui.picker.show(Tmp.c1.set(t.global.colors.brushcolor), true, cons(res => { 
+        t.global.colors.brushcolor = res.rgba(); 
+      })); 
+      Vars.control.input.frag.config.hideConfig(); 
+    })).size(40); 
   },
   configured(tile, player, value){
     //tile.ent().color = value;
