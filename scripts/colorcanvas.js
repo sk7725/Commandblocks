2d8f0669cb0860c658613ea8f86243f593709c4f
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-03 23:38:47 +0900

    ah ah!

--- scripts/colorcanvas.js
+++ scripts/colorcanvas.js
@@ -1,37 +1,59 @@
 var t=this;
-this.global.colors={};//put it in colorcanvas later
-this.global.colors.brushcolor=Color.valueOf("000000").rgba();
-const colorpicker = extendContent(Block, "colorpicker", {
+this.global.colors={};
+const colorcanvas = extendContent(Block, "colorcanvas", {
+  /*
   playerPlaced(tile){
-    tile.configure(t.global.colors.brushcolor); 
+    tile.configure(t.global.colors.brushcolor["U-"+Vars.player.name]);
   },
+  */
   load(){
     this.super$load();
     this.region=Core.atlas.find(this.name);
     this.topRegion=Core.atlas.find(this.name+"-top");
   },
-  draw(tile){ 
-  //super.draw(tile); LightEntity entity = tile.ent(); Draw.blend(Blending.additive); Draw.color(Tmp.c1.set(entity.color), entity.efficiency() * 0.3f); Draw.rect(reg(topRegion), tile.drawx(), tile.drawy()); Draw.color(); Draw.blend(); 
+  draw(tile){
+  //super.draw(tile); LightEntity entity = tile.ent(); Draw.blend(Blending.additive); Draw.color(Tmp.c1.set(entity.color), entity.efficiency() * 0.3f); Draw.rect(reg(topRegion), tile.drawx(), tile.drawy()); Draw.color(); Draw.blend();
   //Tmp.c1.set(tile.ent().color)
   //use in draw
     this.super$draw(tile);
-    Draw.color(Tmp.c1.set(t.global.colors.brushcolor));
+    Draw.color(Tmp.c1.set(t.global.colors.brushcolor["U-"+Vars.player.name]));
     Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
     Draw.color();
   },
   buildConfiguration(tile, table){
-    //var entity = tile.ent(); 
+    //var entity = tile.ent();
     //js-fy this at home
-    table.addImageButton(Icon.pick, run(() => { 
-      Vars.ui.picker.show(Tmp.c1.set(t.global.colors.brushcolor), true, cons(res => { 
-        t.global.colors.brushcolor = res.rgba(); 
-      })); 
-      Vars.control.input.frag.config.hideConfig(); 
-    })).size(40); 
+    table.addImageButton(Icon.pick, run(() => {
+      Vars.ui.picker.show(Tmp.c1.set(t.global.colors.brushcolor["U-"+Vars.player.name]), true, cons(res => {
+        tile.configure(res.rgba());
+      }));
+      Vars.control.input.frag.config.hideConfig();
+    })).size(40);
   },
   configured(tile, player, value){
     //tile.ent().color = value;
-    t.global.colors.brushcolor=value;
+    t.global.colors.brushcolor["U-"+player.name]=value;
   }
   //save load brush
 });
+
+colorcanvas.entityType=prov(() => extendContent(TileEntity , colorcanvas , {
+  config(){
+    return this._color;
+  },
+  write(stream){
+    this.super$write(stream);
+    stream.writeShort(this._color);
+  },
+  read(stream,revision){
+    this.super$read(stream,revision);
+    this._color=stream.readShort();
+  },
+  _color:0,
+  getcolor(){
+    return this._color;
+  },
+  setcolor(item){
+    this._color=item;
+  }
+}));
