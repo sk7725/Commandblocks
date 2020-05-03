4c48b1217df6d785a54cae0bdd224366b5506c91
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-03 22:49:35 +0900

    let me cri

--- scripts/colorpicker.js
+++ scripts/colorpicker.js
@@ -6,11 +6,18 @@ const colorpicker = extendContent(Block, "colorpicker", {
     tile.configure(t.global.colors.brushcolor); 
   },
   load(){
+    this.super$load();
+    this.region=Core.atlas.find(this.name);
+    this.topRegion=Core.atlas.find(this.name+"-top");
   },
   draw(tile){ 
   //super.draw(tile); LightEntity entity = tile.ent(); Draw.blend(Blending.additive); Draw.color(Tmp.c1.set(entity.color), entity.efficiency() * 0.3f); Draw.rect(reg(topRegion), tile.drawx(), tile.drawy()); Draw.color(); Draw.blend(); 
   //Tmp.c1.set(tile.ent().color)
   //use in draw
+    this.super$draw(tile);
+    Draw.color(Tmp.c1.set(t.global.colors.brushcolor));
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+    Draw.color();
   },
   buildConfiguration(tile, table){
     //var entity = tile.ent(); 
