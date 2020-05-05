0a120e21e2d1dba11f33696e16463a0b7d2c417e
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-05 21:50:18 +0900

    oop

--- scripts/playermover.js
+++ scripts/playermover.js
@@ -132,7 +132,7 @@ const playermover=extendContent(MessageBlock,"playermover",{
       var offset=tile.ent().getPos();
       var tileon=Vars.world.tile(tile.x+offset.x,tile.y+offset.y);
       if(tileon.block().name=="commandblocks-buttonpad") tileon.block().unitOn(tileon,null);
-      if(tileon.block().rotate) tile.ent().movePos(((tileon.block().rotation()-1)%2)*-1,((tileon.block().rotation()-2)%2)*-1);
+      if(tileon.block().rotate) tile.ent().movePos(((tileon.rotation()-1)%2)*-1,((tileon.rotation()-2)%2)*-1);
     }
     //TODO:table, draw
 });
