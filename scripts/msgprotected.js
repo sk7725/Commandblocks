1935d7d16ab5fc3be1ca712a0ec75e808998b1bf
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-08 13:58:01 +0900

    Update msgprotected.js

--- scripts/msgprotected.js
+++ scripts/msgprotected.js
@@ -1,5 +1,5 @@
 const msgprotected = extendContent(MessageBlock, "msgprotected", {
 	shouldShowConfigure(tile, player){
-    return !Vars.net.server||player.isAdmin;
+    return (!Vars.net.active())||player.isAdmin;
   }
 });
