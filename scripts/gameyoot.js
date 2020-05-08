f4297de537a709d7f1f7072c4baa6424913ee3a1
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-08 09:32:48 +0900

    oops

--- scripts/gameyoot.js
+++ scripts/gameyoot.js
@@ -170,7 +170,7 @@ const gameyoot=extendContent(MessageBlock,"gameyoot",{
       this.super$update(tile);
       if(tile.ent().message!=""&&tile.ent().timer.check(timerid,landframe*5+80)) return;
 
-      if(tile.ent().message!=""&&tile.ent().timer.check(timerid)==landframe+79){
+      if(tile.ent().message!=""&&tile.ent().timer.getTime(timerid)==landframe+79){
         var outcome=tile.ent().getOutcome();
         var res=this.yootres(tile,outcome);
         //print("Yootres:"+res);
