6290a38bea97d9ee158569461798667f186c0fe5
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-08 13:15:47 +0900

    Update signblock.js

--- scripts/signblock.js
+++ scripts/signblock.js
@@ -1,6 +1,6 @@
 const signblock = extendContent(MessageBlock, "signblock", {
   tapped(tile,player){
-    if(!tile.entity.cons.valid()&&player==Vars.player){
+    if(!tile.entity.cons.valid()&&player==Vars.player&&tile.ent().message!=""){
       Vars.ui.showInfo(tile.ent().message);
     }
   }
