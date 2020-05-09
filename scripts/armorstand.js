1cd15652981000741341defe0c31eb0f70b9f0cc
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-09 21:37:08 +0900

    Update armorstand.js

--- scripts/armorstand.js
+++ scripts/armorstand.js
@@ -73,7 +73,7 @@ armorstand.create(prov(() => new JavaAdapter(GroundUnit, {
     }
 
     //GroundUnit
-    this.stuckTime = !this.vec.set(x, y).sub(this.lastPosition()).isZero(0.0001) ? 0 : this.stuckTime + Time.delta();
+    this.stuckTime = !this.vec.set(this.x, this.y).sub(this.lastPosition()).isZero(0.0001) ? 0 : this.stuckTime + Time.delta();
     if(!this.velocity.isZero()){
       this.baseRotation = Mathf.slerpDelta(this.baseRotation, this.velocity.angle(), 0.05);
     }
