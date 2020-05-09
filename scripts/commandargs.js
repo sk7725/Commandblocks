f6a9f45551d851f20c6f2b8d9ab7bc3343e19167
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-10 00:03:34 +0900

    Update commandargs.js

---
+++ scripts/commandargs.js
@@ -0,0 +1,12 @@
+const commandargs=[
+  {
+    name:"say",
+    params:"<text>",
+    description:"In multiplayer, sends text to all players."
+  },
+  {
+    name:"title",
+    params:"<x> <y> <top/world> <int:duration> <text>",
+    description:"Emits text to all players."
+  }
+]
