3f718de4eba5127cad2f6f50e8561415a482b126
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-18 09:13:57 +0900

    Update keyblock.js

--- scripts/keyblock.js
+++ scripts/keyblock.js
@@ -1,15 +1,20 @@
 const presstick=4; const timerid=0;
+const KeyCode=Packages.arc.input.KeyCode;
 const keyblock = extendContent(MessageBlock, "keyblock", {
   placed(tile) {
     this.super$placed(tile);
-    Call.setMessageBlockText(null,tile,"q");
+    Call.setMessageBlockText(null,tile,"Q");
     //tile.ent().timer.reset(timerid,presstick+1);
   },
-  /*
   draw(tile) {
-    Draw.rect(Core.atlas.find(this.name + ((tile.ent().timer.check(timerid,presstick)) ? "":"-trig")), tile.drawx(), tile.drawy());
+    this.super$draw(tile);
+    //Draw.rect(Core.atlas.find(this.name + (Core.input.keyDown(KeyCode[message])) ? "":"-trig")), tile.drawx(), tile.drawy());
+    this.drawPlaceText(tile.ent().message,tile.x,tile.y,Core.input.keyDown(KeyCode[tile.ent().message]));
   },
-  */
+  setMessageBlockText(player,tile,text){
+    if(!KeyCode.hasOwnProperty(text)) text="ANY_KEY";
+    this.super$setMessageBlockText(player,tile,text.toUpperCase());
+  },
 /*
   update(tile){
     if(tile.ent().timer.getTime(timerid)==presstick) Sounds.click.at(tile.worldx(),tile.worldy(),0.8);
@@ -17,11 +22,11 @@ const keyblock = extendContent(MessageBlock, "keyblock", {
 */
   getPowerProduction(tile){
     try{
-      return (Core.input.keyDown(Packages.arc.input.KeyCode[tile.ent().message])) ? 3: 0;
+      return (Core.input.keyDown(KeyCode[tile.ent().message])) ? 3: 0;
     }
     catch(err){
       print(err);
-      return 0.1;
+      return 0;
     }
-  }
+  },
 });
