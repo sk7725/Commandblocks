dada3bce6558ba4dbaa841d21a8986fdcc5dd299
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-18 09:38:09 +0900

    Update keyblock.js

--- scripts/keyblock.js
+++ scripts/keyblock.js
@@ -11,12 +11,13 @@ const keyblock = extendContent(MessageBlock, "keyblock", {
 
     //Draw.rect(Core.atlas.find(this.name + (Core.input.keyDown(KeyCode[message])) ? "":"-trig")), tile.drawx(), tile.drawy());
     Draw.rect(this.baseRegion, tile.drawx(), tile.drawy());
-    this.drawPlaceText(tile.ent().message,tile.x,tile.y,tile.ent().timer.check(timerid,presstick));
-  },
-  setMessageBlockText(player,tile,text){
-    text=text.toUpperCase();
-    if(!KeyCode.hasOwnProperty(text)) text="ANY_KEY";
-    this.super$setMessageBlockText(player,tile,text);
+    try{
+      this.drawPlaceText(tile.ent().message.toUpperCase(),tile.x,tile.y,Core.input.keyDown(KeyCode[tile.ent().message.toUpperCase()]));
+    }
+    catch(err){
+      this.drawPlaceText("⚠️",tile.x,tile.y,false);
+    }
+
   },
 /*
   update(tile){
@@ -25,20 +26,10 @@ const keyblock = extendContent(MessageBlock, "keyblock", {
 */
   getPowerProduction(tile){
     try{
-      //return (Core.input.keyDown(KeyCode[tile.ent().message])) ? 3: 0;
-      if(Core.input.keyDown(KeyCode[tile.ent().message])){
-        tile.ent().timer.reset(timerid,0);
-        return 1;
-      }
-      else return 0;
+      return (Core.input.keyDown(KeyCode[tile.ent().message.toUpperCase()])) ? 1: 0;
     }
     catch(err){
       //print(err);
-      if(!KeyCode.hasOwnProperty(message)){
-        var message=tile.ent().message.toUpperCase();
-        if(KeyCode.hasOwnProperty(message)) Call.setMessageBlockText(null,tile,message);
-        else Call.setMessageBlockText(null,tile,"ANY_KEY");
-      }
       return 0;
     }
   },
