3aaf2a26b1389d58971a812f6bbfa189d5115ac0
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-18 13:27:39 +0900

    Update gamesoccerball.js

--- scripts/gamesoccerball.js
+++ scripts/gamesoccerball.js
@@ -1,39 +1,35 @@
 var kickpower=10; var dribpower=0.6;
 
 const mechpower={
-  starter:{
-    drib:0.6,
-    kick:10
-  },
-  alpha:{
+  "alpha-mech":{
     drib:0.4,
     kick:13
   },
-  delta:{
+  "delta-mech":{
     drib:1,
     kick:-7
   },
-  tau:{
+  "tau-mech":{
     drib:0.9,
     kick:0
   },
-  omega:{
+  "omega-mech":{
     drib:0.8,
     kick:3
   },
-  trident:{
+  "trident-ship":{
     drib:0.5,
     kick:4
   },
-  glaive:{
+  "glaive-ship":{
     drib:0.3,
     kick:2
   },
-  javelin:{
+  "javelin-ship":{
     drib:0,
     kick:16
   },
-  dart:{
+  "dart-ship":{
     drib:0.6,
     kick:10
   }
@@ -129,7 +125,7 @@ gamesoccerball.create(prov(() => new JavaAdapter(FlyingUnit, {
       var owner=entity.getOwner();
       var dist=Vec2(this.x,this.y).dst2(owner.x,owner.y);
       var power=kickpower;
-      if(owner instanceof Player) power=mechpower[owner.mech.name].kick;
+      if(owner instanceof Player) power=(mechpower.hasOwnProperty(owner.mech.name))?mechpower[owner.mech.name].kick:kickpower;
       if(dist<45000){
         this.velocity().add(power/(this.x-owner.x)*Time.delta(),power/(this.y-owner.y)*Time.delta());
       }
@@ -144,7 +140,10 @@ gamesoccerball.create(prov(() => new JavaAdapter(FlyingUnit, {
     Vars.playerGroup.intersect(cx,cy,fsize,fsize).each(cons(ent => {
       if ((ent instanceof Player)&&ent.getTeam()!=this.getTeam()) {
         this.velocity().add(0.02*(this.x-ent.x)*Time.delta(),0.02*(this.y-ent.y)*Time.delta());
-        this.velocity().add(mechpower[ent.mech.name].drib*(ent.velocity().x)*Time.delta(),mechpower[ent.mech.name].drib*(ent.velocity().y)*Time.delta());
+        this.velocity().add(
+          ((mechpower.hasOwnProperty(ent.mech.name))?mechpower[ent.mech.name].drib:dribpower)*(ent.velocity().x)*Time.delta() ,
+          ((mechpower.hasOwnProperty(ent.mech.name))?mechpower[ent.mech.name].drib:dribpower)*(ent.velocity().y)*Time.delta()
+        );
       }
     }));
   }
