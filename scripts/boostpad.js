e126e88103a8d376e0ee2a9fe6ddf5402880c630
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-19 10:05:52 +0900

    Update boostpad.js

--- scripts/boostpad.js
+++ scripts/boostpad.js
@@ -1,11 +1,25 @@
 const presstick=20; const timerid=0; const blocksize=Vars.tilesize*1.5;
 const animspeed=40;
+const effectdur=140;
 const boostcolor=Color.valueOf("ffbb77");
 const boostblock = newEffect(20, e => {
   Draw.color(boostcolor);
   Lines.stroke(e.fout() * 4);
   Lines.square(e.x, e.y, e.fin() * 11.5);
 });
+
+const boostfire = newEffect(50, e => {
+  var len = e.finpow() * 10;
+  var ang = e.rotation + 180 + Mathf.randomSeedRange(e.id, 30);
+  Draw.color(boostcolor, Pal.lightOrange, e.fin());
+  Fill.circle(e.x + Angles.trnsx(ang, len), e.y + Angles.trnsy(ang, len), 2 * e.fout());
+});
+
+const boosted= extendContent(StatusEffect,"boosted",{});
+boosted.speedMultiplier=2;
+boosted.color=boostcolor;
+boosted.effect=boostfire;
+
 const boostpad = extendContent(Block, "boostpad", {
   placed(tile) {
     this.super$placed(tile);
@@ -30,6 +44,7 @@ const boostpad = extendContent(Block, "boostpad", {
     }
     tile.ent().timer.reset(timerid,0);
     unit.velocity().add(unit.velocity().x*Time.delta()*3,unit.velocity().y*Time.delta()*3);
+    unit.applyEffect(boosted,effectdur);
   },
   update(tile){
     this.super$update(tile);
