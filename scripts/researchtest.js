d30b4316f87ace4fd0956302f63c871d69f0a0a9
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-25 11:52:22 +0900

    Update researchtest.js

--- scripts/researchtest.js
+++ scripts/researchtest.js
@@ -12,7 +12,7 @@ const researchtest = extendContent(MessageBlock, "researchtest", {
 		var entity=tile.ent();
 		table.addImageButton(Icon.book, run(() => {
       try{
-				const dialog = new customtree(root);
+				const dialog = new customtree.FloatingDialog(root);
 				// Show it
 				dialog.show();
       }
