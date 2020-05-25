b0407018cfa3d5370a883b97f78d0c4d64e02b26
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-25 12:28:41 +0900

    Update customtechtree.js

--- scripts/customtechtree.js
+++ scripts/customtechtree.js
@@ -5,7 +5,7 @@ const customtree=extendContent(FloatingDialog,"CustomTreeDialog",{
     this.root=root;
     this.cbounds=new Rect();
     this.items=new ItemsDisplay();
-    this.view=new TechTreeDialog.View();
+    this.view=new TechTreeDialog.View;
 
     this.super("");
     this.titleTable.remove();
