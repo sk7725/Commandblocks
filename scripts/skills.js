ba48b2b7754c910ac7def17c9858ec04933ac3e4
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-05-29 00:59:33 +0900

    Update skills.js

--- scripts/skills.js
+++ scripts/skills.js
@@ -599,7 +599,7 @@ const gravsuck = newEffect(20, e => {
 const gravstart = newEffect(45, e => {
   Draw.color(zetacolor);
   Lines.stroke(3*e.fout()+0.001);
-	var sx=e.x+(e.data.x)*e.fout(); var sy=e.y+(e.data.y)*e.fout();
+	//var sx=e.x+(e.data.x)*e.fout(); var sy=e.y+(e.data.y)*e.fout();
   Lines.square(e.x, e.y, (e.fin()>0.78)?(e.fin()-0.78)*400+5:5 ,(e.fout()*720)%360);
 });
 const gravityTrap=extend(BasicBulletType,{
