0c02d5674c4eaeac6ad10eb7da911596248f9e56
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-05-30 22:28:15 +0900

    ???

--- scripts/skills.js
+++ scripts/skills.js
@@ -731,14 +731,14 @@ const swordeffect = newEffect(7, e => {
   var width=4;
   var r1=sign*(45-30*e.fin());
   var v1=Vec2(10,0).setAngle(e.rotation+r1);
-  Drawf.tri(e.x+v1.x, e.y+v1.y, width, 2.8*e.fin(), (e.rotation-90+r1)%360);
+  Drawf.tri(e.x+v1.x, e.y+v1.y, width, 2.8*e.fin(), (e.rotation+90+r1)%360);
   r1=sign*(45-45*e.fin());
   v1=Vec2(10,0).setAngle(e.rotation+r1);
   Lines.stroke(width);
-  Lines.lineAngleCenter(e.x+v1.x, e.y+v1.y, (e.rotation+90+r1)%360, 5.6*e.fin());
+  //Lines.lineAngleCenter(e.x+v1.x, e.y+v1.y, (e.rotation+90+r1)%360, 5.6*e.fin());
   r1=sign*(45-60*e.fin());
   v1=Vec2(10,0).setAngle(e.rotation+r1);
-  Drawf.tri(e.x+v1.x, e.y+v1.y, width, 2.8*e.fin(), (e.rotation+90+r1)%360);
+  //Drawf.tri(e.x+v1.x, e.y+v1.y, width, 2.8*e.fin(), (e.rotation-90+r1)%360);
 });
 
 const boostedskill= extendContent(StatusEffect,"boostedskill",{});
