391cb2fa92573ebbeae0fb7210c48f50284f30c1
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-02 00:20:12 +0900

    Update shadertester.js

--- scripts/shadertester.js
+++ scripts/shadertester.js
@@ -38,3 +38,6 @@ const shadertester=extendContent(MessageBlock,"shadertester",{
     Call.setMessageBlockText(null,tile,"#ifdef GL_ES\nprecision mediump float;precision mediump int;\n#endif\nuniform sampler2D u_texture;uniform float u_time;varying vec4 v_color;varying vec2 v_texCoord;void main(){vec4 color = texture2D(u_texture, v_texCoord.xy);float t = clamp((sin(u_time * .01 + gl_FragCoord.x * .01 + gl_FragCoord.y * .005) + 1.) / 2., 0., 1.);vec3 c = vec3(mix(0., 1., t), mix(.89, .39, t), mix(1., .85, t));gl_FragColor = vec4(color.rgb * c.rgb, color.a);}");
 	}
 });
+
+shadertester.maxTextLength=700;
+shadertester.maxNewlines=50;
