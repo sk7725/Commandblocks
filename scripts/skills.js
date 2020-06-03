d3d6997082b0029c4d65528c8443f24b18e8772f
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-03 13:57:27 +0900

    jjjjjjjjjjjjjjjjjjjjjjjj

--- scripts/skills.js
+++ scripts/skills.js
@@ -808,7 +808,7 @@ const shieldsmall = extendContent(StatusEffect,"shieldsmall",{
   }
 });
 shieldsmall.color = Color.valueOf("ffd37f");
-shieldsmall.trans(shieldbreak, handle((unit, time, newTime, result) => {
+shieldsmall.trans(shieldbreak, StatusEffect.TransitionHandler.handle((unit, time, newTime, result) => {
   result.set(shieldbreak, newTime);
 }));
 
