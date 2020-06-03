c2ac6f2e962e9a35ba73049b82b54455661a1c73
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-06-03 19:31:16 +0900

    hm

--- scripts/shadertester.js
+++ scripts/shadertester.js
@@ -8,6 +8,7 @@ var shader = new JavaAdapter(Shader, {
 "uniform mat4 u_projTrans;attribute vec4 a_position;attribute vec2 a_texCoord0;attribute vec4 a_color;varying vec4 v_color;varying vec2 v_texCoord;void main(){gl_Position = u_projTrans * a_position;v_texCoord = a_texCoord0;v_color = a_color;}", "#ifdef GL_ES\nprecision mediump float;precision mediump int;\n#endif\nuniform sampler2D u_texture;uniform float u_time;varying vec4 v_color;varying vec2 v_texCoord;void main(){vec4 color = texture2D(u_texture, v_texCoord.xy);float t = clamp((sin(u_time * .01 + gl_FragCoord.x * .01 + gl_FragCoord.y * .005) + 1.) / 2., 0., 1.);vec3 c = vec3(mix(0., 1., t), mix(.89, .39, t), mix(1., .85, t));gl_FragColor = vec4(color.rgb * c.rgb, color.a);}");
 const shadertester=extendContent(MessageBlock,"shadertester",{
   draw(tile){
+    if(shader == null) return;
     Draw.shader(shader);
     Draw.rect(this.region, tile.drawx(), tile.drawy());
     Draw.shader();
@@ -53,16 +54,22 @@ const shadertester=extendContent(MessageBlock,"shadertester",{
     })).size(40);
   },
   configured(tile,player, value){
-    if(value!=1) return;
+    if(value != 1) return;
     var msg=tile.ent().message;
-    shader.dispose();
-    shader = new JavaAdapter(Shader, {
-      apply(){
-        this.setUniformf("u_time", Time.time() / Scl.scl(1.0));
-      }
-    },
-    //todo make multiline strings work
-    "uniform mat4 u_projTrans;attribute vec4 a_position;attribute vec2 a_texCoord0;attribute vec4 a_color;varying vec4 v_color;varying vec2 v_texCoord;void main(){gl_Position = u_projTrans * a_position;v_texCoord = a_texCoord0;v_color = a_color;}", msg);
+    if(shader != null) shader.dispose();
+    try{
+      shader = new JavaAdapter(Shader, {
+        apply(){
+          this.setUniformf("u_time", Time.time() / Scl.scl(1.0));
+        }
+      },
+      //todo make multiline strings work
+      "uniform mat4 u_projTrans;attribute vec4 a_position;attribute vec2 a_texCoord0;attribute vec4 a_color;varying vec4 v_color;varying vec2 v_texCoord;void main(){gl_Position = u_projTrans * a_position;v_texCoord = a_texCoord0;v_color = a_color;}", msg);
+    }
+    catch(err){
+      Call.sendMessage(err);
+      shader = null;
+    }
   },
   placed(tile){
 		this.super$placed(tile);
