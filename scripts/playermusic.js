b2d51de9884846022324d4437dd1a70ee7b86350
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-05 23:53:29 +0900

    please.

--- scripts/playermusic.js
+++ scripts/playermusic.js
@@ -1,9 +1,53 @@
-const FileChooser=Packages.mindustry.ui.dialogs.FileChooser;
+//const FileChooser=Packages.mindustry.ui.dialogs.FileChooser;
+if (typeof(cons2)== "undefined"){
+  const cons2 = method => new Cons2(){get : method};
+}
 
-const playermusic = extendContent(MessageBlock, "playermusic", {
+const playermusic = extendContent(Block, "playermusic", {
   music:null,
+  musics:[],
+  custom:false,
   buildConfiguration(tile, table){
     const entity = tile.ent();
+
+    table.addImageButton(Icon.play, run(() => {
+      try{
+        if(this.music != null && this.custom) this.music.play();
+        else this.musics[tile.ent().getVal()-1].play();
+      }
+      catch(err){
+        print(err);
+      }
+    })).size(40);
+    table.addImageButton(Icon.pause, run(() => {
+      try{
+        if(this.music != null && this.custom) this.music.pause();
+        else this.musics[tile.ent().getVal()-1].pause();
+      }
+      catch(err){
+        print(err);
+      }
+    })).size(40);
+    table.addImageButton(Icon.block, run(() => {
+      try{
+        if(this.music != null && this.custom) this.music.stop();
+        else this.musics[tile.ent().getVal()-1].stop();
+      }
+      catch(err){
+        print(err);
+      }
+    })).size(40);
+    table.row();
+
+    table.addImageButton(Icon.upOpen, run(() => {
+      if(tile.ent().getVal()<this.musics.length) tile.configure(-1);
+      this.custom = false;
+    })).size(40);
+    table.addImageButton(Icon.downOpen, run(() => {
+      if(tile.ent().getVal()>1) tile.configure(-2);
+      this.custom = false;
+    })).size(40);
+
     table.addImageButton(Icon.file, run(() => {
       // Create dialog
       try{
@@ -26,6 +70,7 @@ const playermusic = extendContent(MessageBlock, "playermusic", {
             if(this.music != null) this.music.dispose();
             this.music = Core.audio.newMusic(f);
             this.music.setVolume(1);
+            this.custom = true;
           }
           catch(err){
             this.music = null;
@@ -37,38 +82,71 @@ const playermusic = extendContent(MessageBlock, "playermusic", {
         print(err.stack);
       }
     })).size(40);
-    table.addImageButton(Icon.play, run(() => {
-      try{
-        if(this.music != null) this.music.play();
+  },
+  configured(tile, player, value){
+    if(value==-1||value==-2){
+      if(value==-1){
+        tile.ent().addVal(1);
       }
-      catch(err){
-        print(err);
+      else{
+        tile.ent().addVal(-1);
       }
-    })).size(40);
-    table.addImageButton(Icon.pause, run(() => {
-      try{
-        if(this.music != null) this.music.pause();
-      }
-      catch(err){
-        print(err);
-      }
-    })).size(40);
-    table.addImageButton(Icon.block, run(() => {
-      try{
-        if(this.music != null) this.music.stop();
-      }
-      catch(err){
-        print(err);
-      }
-    })).size(40);
+      Vars.ui.showInfoToast("Now Playing: "+tile.ent().getVal(),3);
+    }
+    else if(value>0&&value<=this.musics.length) tile.ent().setVal(value);
   },
   update(tile){
     if(tile.entity.cons.valid()){
-      if(this.music == null) return;
-      if(!this.music.isPlaying()) this.music.play();
+      if(this.music != null && this.music.isPlaying()) return;
+      try{
+        var val = tile.ent().getVal()-1;
+        for(var i=0;i<this.musics.length;i++){
+          if(this.musics[i].isPlaying() && i != val) this.musics[i].stop();
+        }
+        if(!this.musics[val].isPlaying()) this.musics[val].play();
+      }
     }
+  },
+  load(){
+    this.super$load();
+    if(this.musics.length>0){
+      for(var i=0;i<this.musics.length;i++){
+        try{
+          this.musics[i].dispose();
+        }
+        catch(err){}
+      }
+    }
+    Vars.mods.listFiles("music", cons2((l, f)=>{
+      this.musics.push(Core.audio.newMusic(f));
+    }));
   }
 });
+
+playermusic.entityType=prov(() => extend(TileEntity, {
+  _now:1,
+  config(){
+    return this._now;
+    //saves pulse in schems
+  },
+  getVal(){
+    return this._now;
+  },
+  setVal(a){
+    this._now = a;
+  },
+  addVal(a){
+    this._now += a;
+  },
+  write(stream){
+    this.super$write(stream);
+    stream.writeShort(this._now);
+  },
+  read(stream,revision){
+    this.super$read(stream,revision);
+    this._now=stream.readShort();
+  }
+}));
 //var muselist={};
 /*
 const playermusic = extendContent(MessageBlock, "playermusic", {
