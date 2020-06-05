a01244bf06c0e1e7d543a1775891b542ffe92b1d
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-05 10:25:05 +0900

    aaaaaaaaaaaaa

--- scripts/command.js
+++ scripts/command.js
@@ -1187,7 +1187,7 @@ const commandblocks={
             if(args.length>=6) vel=args[5];
             if(args.length>=7) life=args[6];
             if(args.length>=8&&args[7]=="true") bind=true;
-            if(args.length>=9) cdata=args[9];
+            if(args.length>=9) cdata=args[8];
             this.cmdfire(tile,args[0],cx,cy,crot,cteam,vel,life,bind,cdata);
           }
           else throw "Coordinates should be above 0";
