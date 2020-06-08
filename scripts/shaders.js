9fc4c5cb34bf693afa2c85cb891729bb3b95fdc5
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-08 13:49:14 +0900

    !

--- scripts/shaders.js
+++ scripts/shaders.js
@@ -29,7 +29,7 @@ if(!Vars.headless){
       }
     },
     //todo make multiline strings work
-    "uniform mat4 u_projTrans;attribute vec4 a_position;attribute vec2 a_texCoord0;attribute vec4 a_color;varying vec4 v_color;varying vec2 v_texCoord;void main(){gl_Position = u_projTrans * a_position;v_texCoord = a_texCoord0;v_color = a_color;}", "#ifdef GL_ES\nprecision highp float;\n#endif\n#define PI 3.14159265;\nuniform sampler2D u_texture;\nvarying vec4 v_color;\nvarying vec2 v_texCoord;\nuniform float u_time;\nuniform vec2 u_resolution;\nvoid main( void ) {\nvec4 color = texture2D(u_texture, v_texCoord.xy);\nfloat time = u_time*0.0001;\nfloat color1, color2, color3;\ncolor1 = (sin(dot(gl_FragCoord.xy,vec2(sin(time*1.0),cos(time*3.0)))*0.02+time*4.0)+1.0)/2.0;\nvec2 center = vec2(u_resolution.x, u_resolution.y) + vec2(u_resolution.x/2.0*sin(-time*3.0),u_resolution.y/2.0*cos(-time*10.0));\ncolor2 = (cos(length(gl_FragCoord.xy - center)*0.03)+1.0)/2.0;\ncolor = (color1+ color2)/2.0;\nfloat red   = (cos(PI*color3/0.5+time*3.0)+1.0)/2.0;\nfloat green   = (sin(PI*color3/0.5+time*3.0)+1.0)/2.0;\nfloat blue   = (sin(PI*color3/0.25+time*3.0)+1.0)/2.0;\nvec3 fin = vec3(blue+0.6, red+0.5, green+0.5);\ngl_FragColor = vec4(color.rgb * fin, color.a);\n}");
+    "uniform mat4 u_projTrans;attribute vec4 a_position;attribute vec2 a_texCoord0;attribute vec4 a_color;varying vec4 v_color;varying vec2 v_texCoord;void main(){gl_Position = u_projTrans * a_position;v_texCoord = a_texCoord0;v_color = a_color;}", "#ifdef GL_ES\nprecision highp float;\n#endif\n#define PI 3.14159265\nuniform sampler2D u_texture;\nvarying vec4 v_color;\nvarying vec2 v_texCoord;\nuniform float u_time;\nuniform vec2 u_resolution;\nvoid main( void ) {\nvec4 color = texture2D(u_texture, v_texCoord.xy);\nfloat time = u_time*0.0001;\nfloat color1, color2, color3;\ncolor1 = (sin(dot(gl_FragCoord.xy,vec2(sin(time*1.0),cos(time*3.0)))*0.02+time*4.0)+1.0)/2.0;\nvec2 center = vec2(u_resolution.x, u_resolution.y) + vec2(u_resolution.x/2.0*sin(-time*3.0),u_resolution.y/2.0*cos(-time*10.0));\ncolor2 = (cos(length(gl_FragCoord.xy - center)*0.03)+1.0)/2.0;\ncolor = (color1+ color2)/2.0;\nfloat red   = (cos(PI*color3/0.5+time*3.0)+1.0)/2.0;\nfloat green   = (sin(PI*color3/0.5+time*3.0)+1.0)/2.0;\nfloat blue   = (sin(PI*color3/0.25+time*3.0)+1.0)/2.0;\nvec3 fin = vec3(blue+0.6, red+0.5, green+0.5);\ngl_FragColor = vec4(color.rgb * fin, color.a);\n}");
     this.global.shaders.time=shader3;
   }
   catch(err){
@@ -48,7 +48,7 @@ if(!Vars.headless){
           }
         },
         //todo make multiline strings work
-        "uniform mat4 u_projTrans;attribute vec4 a_position;attribute vec2 a_texCoord0;attribute vec4 a_color;varying vec4 v_color;varying vec2 v_texCoord;void main(){gl_Position = u_projTrans * a_position;v_texCoord = a_texCoord0;v_color = a_color;}", "#ifdef GL_ES\nprecision mediump float;\n#endif\n#define PI 3.14159265;\nuniform sampler2D u_texture;\nvarying vec4 v_color;\nvarying vec2 v_texCoord;\nuniform float u_time;\nuniform vec2 u_resolution;\nvoid main( void ) {\nvec4 color = texture2D(u_texture, v_texCoord.xy);\nfloat time = u_time*0.0001;\nfloat color1, color2, color3;\ncolor1 = (sin(dot(gl_FragCoord.xy,vec2(sin(time*1.0),cos(time*3.0)))*0.02+time*4.0)+1.0)/2.0;\nvec2 center = vec2(u_resolution.x, u_resolution.y) + vec2(u_resolution.x/2.0*sin(-time*3.0),u_resolution.y/2.0*cos(-time*10.0));\ncolor2 = (cos(length(gl_FragCoord.xy - center)*0.03)+1.0)/2.0;\ncolor = (color1+ color2)/2.0;\nfloat red   = (cos(PI*color3/0.5+time*3.0)+1.0)/2.0;\nfloat green   = (sin(PI*color3/0.5+time*3.0)+1.0)/2.0;\nfloat blue   = (sin(PI*color3/0.25+time*3.0)+1.0)/2.0;\nvec3 fin = vec3(blue+0.6, red+0.5, green+0.5);\ngl_FragColor = vec4(color.rgb * fin, color.a);\n}");
+        "uniform mat4 u_projTrans;attribute vec4 a_position;attribute vec2 a_texCoord0;attribute vec4 a_color;varying vec4 v_color;varying vec2 v_texCoord;void main(){gl_Position = u_projTrans * a_position;v_texCoord = a_texCoord0;v_color = a_color;}", "#ifdef GL_ES\nprecision mediump float;\n#endif\n#define PI 3.14159265\nuniform sampler2D u_texture;\nvarying vec4 v_color;\nvarying vec2 v_texCoord;\nuniform float u_time;\nuniform vec2 u_resolution;\nvoid main( void ) {\nvec4 color = texture2D(u_texture, v_texCoord.xy);\nfloat time = u_time*0.0001;\nfloat color1, color2, color3;\ncolor1 = (sin(dot(gl_FragCoord.xy,vec2(sin(time*1.0),cos(time*3.0)))*0.02+time*4.0)+1.0)/2.0;\nvec2 center = vec2(u_resolution.x, u_resolution.y) + vec2(u_resolution.x/2.0*sin(-time*3.0),u_resolution.y/2.0*cos(-time*10.0));\ncolor2 = (cos(length(gl_FragCoord.xy - center)*0.03)+1.0)/2.0;\ncolor = (color1+ color2)/2.0;\nfloat red   = (cos(PI*color3/0.5+time*3.0)+1.0)/2.0;\nfloat green   = (sin(PI*color3/0.5+time*3.0)+1.0)/2.0;\nfloat blue = (sin(PI*color3/0.25+time*3.0)+1.0)/2.0;\nvec3 fin = vec3(blue+0.6, red+0.5, green+0.5);\ngl_FragColor = vec4(color.rgb * fin, color.a);\n}");
         this.global.shaders.time=lowres;
       }
       catch(err){
@@ -85,20 +85,20 @@ gl_FragColor = vec4(color.rgb * fin, color.a);
 #ifdef GL_ES
 precision highp float;
 #endif
-#define PI 3.14159265;
+#define PI 3.14159265
 uniform sampler2D u_texture;
 varying vec4 v_color;
 varying vec2 v_texCoord;
 uniform float u_time;
 uniform vec2 u_resolution;
 void main( void ) {
-vec4 color3 = texture2D(u_texture, v_texCoord.xy);
+vec4 color = texture2D(u_texture, v_texCoord.xy);
 float time = u_time*0.0001;
 float color1, color2, color3;
 color1 = (sin(dot(gl_FragCoord.xy,vec2(sin(time*1.0),cos(time*3.0)))*0.02+time*4.0)+1.0)/2.0;
 vec2 center = vec2(u_resolution.x, u_resolution.y) + vec2(u_resolution.x/2.0*sin(-time*3.0),u_resolution.y/2.0*cos(-time*10.0));
 color2 = (cos(length(gl_FragCoord.xy - center)*0.03)+1.0)/2.0;
-color3 = (color1+ color2)/2.0;
+color3 = (color1 + color2)/2.0;
 float red   = (cos(PI*color3/0.5+time*3.0)+1.0)/2.0;
 float green   = (sin(PI*color3/0.5+time*3.0)+1.0)/2.0;
 float blue   = (sin(PI*color3/0.25+time*3.0)+1.0)/2.0;
