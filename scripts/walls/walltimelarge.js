62702c8bc60dd83acd5a2bff1118dfad08dca755
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2020-06-09 17:29:24 +0200

    Update walltimelarge.js

--- scripts/walls/walltimelarge.js
+++ scripts/walls/walltimelarge.js
@@ -1,5 +1,11 @@
 const shader=this.global.shaders.time;
-
+const darknessbullet = extend(BasicBulletType, {
+    draw(b){
+      Draw.color("64e39b");
+      Fill.circle(b.x, b.y, 10)
+      Draw.color();
+    }
+});
 const walltimelarge=extendContent(Wall,"walltimelarge",{
   draw(tile){
     Draw.shader(shader);
@@ -10,5 +16,10 @@ const walltimelarge=extendContent(Wall,"walltimelarge",{
     this.super$load();
     this.region=Core.atlas.find(this.name);
     this.animRegion=Core.atlas.find(this.name+"-anim");
+  },
+  onDestroyed(tile){
+    for(i = 0;i<721;i++){
+      Calls.createBullet(darknessbullet, tile.getTeam(), tile.drawx(), tile.drawy(), (i*-1)+360, Mathf.random(0.5, 1.0), Mathf.random(0.2, 1.0));
+    }
   }
 });
