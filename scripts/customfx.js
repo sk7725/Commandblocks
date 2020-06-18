d357870abc8e9911f57cb305ee2449ec08796d0a
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-18 10:50:48 +0900

    Update customfx.js

--- scripts/customfx.js
+++ scripts/customfx.js
@@ -176,8 +176,8 @@ this.global.fx = {
     Draw.color(Color.black);
 		Fill.circle(e.x, e.y, e.rotation);
   }),
-  poof : newEffect(85, e => {
-    var v1 = Vec2((1-e.fin()*e.fin())*30,0);
+  poof : newEffect(65, e => {
+    var v1 = Vec2((1-e.fout()*e.fout())*30,0);
     var r1 = e.fout()*270;
     var c1 = e.fout()*45; if(c1>3.5) c1=3.5;
     Draw.color(Color.black);
@@ -191,8 +191,8 @@ this.global.fx = {
       Fill.circle(e.x+v1.x, e.y+v1.y, c1);
     }
   }),
-  poofBack : newEffect(85, e => {
-    var v1 = Vec2((1-e.fout()*e.fout())*30,0);
+  poofBack : newEffect(65, e => {
+    var v1 = Vec2((1-e.fin()*e.fin())*30,0);
     var r1 = e.fout()*270;
     var c1 = e.fin()*45; if(c1>3.5) c1=3.5;
     Draw.color(Color.black);
