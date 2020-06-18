3637064c803e809ece59e4848207fd0e53193eeb
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-18 10:22:50 +0900

    Update customfx.js

--- scripts/customfx.js
+++ scripts/customfx.js
@@ -177,30 +177,30 @@ this.global.fx = {
 		Fill.circle(e.x, e.y, e.rotation);
   }),
   poof : newEffect(120, e => {
-    var v1 = Vec2(1-e.fout()*e.fout()*30,0);
-    var r1 = e.fout()*150;
-    var c1 = e.fin()*15; if(c1>5) c1=5;
+    var v1 = Vec2(1-e.fin()*e.fin()*30,0);
+    var r1 = e.fout()*270;
+    var c1 = e.fout()*45; if(c1>3.5) c1=3.5;
     Draw.color(Color.black);
     for(var i=0;i<8;i++){
       v1.setAngle((r1+i*45)%360);
       Fill.circle(e.x+v1.x, e.y+v1.y, c1+1);
     }
-    Draw.color((Time.time()%40>20)?Color.white:e.color);
+    Draw.color((Time.time()%30>15)?Color.white:e.color);
     for(var i=0;i<8;i++){
       v1.setAngle((r1+i*45)%360);
       Fill.circle(e.x+v1.x, e.y+v1.y, c1);
     }
   }),
   poofBack : newEffect(120, e => {
-    var v1 = Vec2(1-e.fin()*e.fin()*30,0);
-    var r1 = e.fout()*150;
-    var c1 = e.fin()*15; if(c1>5) c1=5;
+    var v1 = Vec2(1-e.fout()*e.fout()*30,0);
+    var r1 = e.fout()*270;
+    var c1 = e.fout()*45; if(c1>3.5) c1=3.5;
     Draw.color(Color.black);
     for(var i=0;i<8;i++){
       v1.setAngle((r1+i*45)%360);
       Fill.circle(e.x+v1.x, e.y+v1.y, c1+1);
     }
-    Draw.color((Time.time()%40>20)?Color.white:e.color);
+    Draw.color((Time.time()%30>15)?Color.white:e.color);
     for(var i=0;i<8;i++){
       v1.setAngle((r1+i*45)%360);
       Fill.circle(e.x+v1.x, e.y+v1.y, c1);
