b908b5bf15ef13e5bcc5ce76f4096fd057c004ec
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-06-26 18:10:25 +0900

    yu

--- scripts/commandb.js
+++ scripts/commandb.js
@@ -193,7 +193,7 @@ const commandb = extendContent(MessageBlock, "commandb", {
     var result = new StringBuilder(text.length());
     text = text.trim();
     var count = 0;
-    for(int i = 0; i < text.length(); i++){
+    for(var i = 0; i < text.length(); i++){
         var c = text.charAt(i);
         if(c == '\n' || c == '\r'){
             count ++;
