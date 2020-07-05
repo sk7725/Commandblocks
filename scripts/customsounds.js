5adaf44d3a91b44a2a1209d3864fab7f1fc7726a
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-07-05 20:04:19 +0900

    Update customsounds.js

--- scripts/customsounds.js
+++ scripts/customsounds.js
@@ -4,14 +4,25 @@ var t = this;
 
 function loadsound(name){
   try{
-    (Core.assets.load("sounds/commandblocks/"+ name +".ogg", Packages.arc.audio.Sound)).loaded = cons(a => {
-      t.global.newSounds[name] = a;
+    (Core.assets.load("sounds/"+ name +".ogg", Packages.arc.audio.Sound)).loaded = cons(a => {
+      try{
+        t.global.newSounds[name] = a;
+      }
+      catch(err){
+        t.global.newSounds[name] = Sounds.none;
+        print("Failed to load sound!");
+        print(err);
+      }
     });
   }
   catch(err){
     t.global.newSounds[name] = Sounds.none;
     print("Failed to load sound!");
     print(err);
+  }
+  if(!t.global.newSounds[name]){
+    t.global.newSounds[name] = Sounds.none;
+    print("Failed to load sound! Please restart the game!");
   }
 }
 
