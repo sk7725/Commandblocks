a3a11930e15141731635f81c461253f0a7646e17
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-07-05 12:54:17 +0900

    Update explosive.js

--- scripts/campaign/explosive.js
+++ scripts/campaign/explosive.js
@@ -6,7 +6,7 @@ const explosive = extendContent(Block, "explosive", {
   },
   draw(tile) {
     this.super$draw(tile);
-    //Draw.rect(Core.atlas.find(this.name + ((tile.ent().timer.check(timerid,presstick)) ? "":"-trig")), tile.drawx(), tile.drawy());
+    if(tile.ent().fuse() && tile.ent().timer.getTime(timerid)%45<22.5) Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
   },
   tapped(tile,player){
     tile.ent().timer.reset(timerid, 0);
@@ -15,11 +15,33 @@ const explosive = extendContent(Block, "explosive", {
   },
   update(tile){
     if(tile.ent().fuse()) this.updateFuse(tile);
-    else if(tile.ent().cons.valid){
+    else if(tile.ent().cons.valid()){
       tile.ent().timer.reset(timerid, 0);
       tile.ent().lit();
     }
   },
   updateFuse(tile){
+  },
+  load(){
+    this.super$load();
+    this.topRegion = Core.atlas.find(this.name + "-top");
   }
 });
+
+explosive.entityType=prov(() => extend(TileEntity, {
+  _trig:false,
+  fuse(){
+    return this._trig;
+  },
+  lit(){
+    this._trig = true;
+  },
+  write(stream){
+    this.super$write(stream);
+    stream.writeBoolean(this._trig);
+  },
+  read(stream,revision){
+    this.super$read(stream,revision);
+    this._trig=stream.readBoolean();
+  }
+}));
