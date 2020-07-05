ad6adfd5f6103b73a25c5481b58d0d78c5d445bf
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-07-05 19:49:28 +0900

    yup

--- scripts/customsounds.js
+++ scripts/customsounds.js
@@ -1,1 +1,18 @@
 this.global.newSounds = {};
+
+var t = this;
+
+function loadsound(name){
+  try{
+    (Core.assets.load("sounds/commandblocks/"+ name +".ogg", Packages.arc.audio.Sound)).loaded = cons(a => {
+      t.global.newSounds[name] = a;
+    });
+  }
+  catch(err){
+    print("Failed to load sound!");
+    print(err);
+  }
+}
+
+loadsound("boostsound");
+loadsound("tntfuse");
