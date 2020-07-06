b62b380cdb4af9e21aafabc7430e6092f372cbf1
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-07-06 14:44:44 +0900

    Update ores.js

--- scripts/campaign/ores.js
+++ scripts/campaign/ores.js
@@ -22,7 +22,7 @@ function drawBit(bit, x, y, size, stroke){
 
 const scalarFx = newEffect(45, e => {
   Draw.color(colors.scalar);
-  Angles.randLenVectors(e.id, 3, 7+5*e.fin(), floatc2((x,y) => {
+  Angles.randLenVectors(e.id, 2, 7+5*e.fin(), floatc2((x,y) => {
     Fill.circle(e.x+x, e.y+y, e.fout()*0.9);
   }));
 });
@@ -32,17 +32,19 @@ const vectorFx = newEffect(45, e => {
     Drawf.tri(e.x+x, e.y+y, e.fout()*2.7, e.fout()*2.7, e.x%5*36*e.fin());
   }));
 });
-const zetaFx = newEffect(45, e => {
+const zetaFx = newEffect(50, e => {
   Draw.color(colors.zeta);
   Lines.stroke(e.fout());
-  Angles.randLenVectors(e.id, 5, 7+5*e.fin(), floatc2((x,y) => {
-    Lines.poly(e.x+x, e.y+y, 4, e.fin()*0.6+0.3);
+  Angles.randLenVectors(e.id, 4, 7+5*e.fin(), floatc2((x,y) => {
+    Lines.poly(e.x+x, e.y+y, 4, e.fin()*0.6+0.45);
   }));
 });
-const codeFx = newEffect(45, e => {
+const codeFx = newEffect(50, e => {
   Draw.color(colors.code);
-  Angles.randLenVectors(e.id, 6, 7+5*e.fin(), floatc2((x,y) => {
-    drawBit(e.id%2, e.x+x, e.y+y, 1, e.fout());
+  var i=e.id;
+  Angles.randLenVectors(e.id, 5, 7+5*e.fin(), floatc2((x,y) => {
+    i++;
+    drawBit(i%2, e.x+x, e.y+y, 1, e.fout());
   }));
 });
 
@@ -66,11 +68,12 @@ const depocode = extendContent(OreBlock, "depo-code", {
 */
 const sparkling = ["commandblocks-depo-scalar", "commandblocks-depo-vector", "commandblocks-depo-zeta", "commandblocks-depo-code"];
 const sparkleFx = [scalarFx, vectorFx, zetaFx, codeFx];
+const thresh = (Vars.mobile)?2:4;
 
 var v1 = new Vec2(0,0); var tile = null;
 Events.on(EventType.Trigger.update, run(function(){
   if(Vars.state.is(GameState.State.playing)){
-    for(var i=0;i<4;i++){
+    for(var i=0;i<thresh;i++){
       v1 = Core.camera.unproject(Mathf.random()*Core.graphics.getWidth(), Mathf.random()*Core.graphics.getHeight());
       tile = Vars.world.tileWorld(v1.x, v1.y);
       if(tile!=null && sparkling.indexOf(tile.overlay().name)>-1) Effects.effect(sparkleFx[sparkling.indexOf(tile.overlay().name)], tile.worldx(), tile.worldy());
