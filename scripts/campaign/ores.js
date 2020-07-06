03ec46c58e9a4119fe1625188a6a8239646ecbf9
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-07-06 13:29:14 +0900

    dammit

--- scripts/campaign/ores.js
+++ scripts/campaign/ores.js
@@ -20,61 +20,56 @@ function drawBit(bit, x, y, size, stroke){
   }
 }
 
-const scalarFx = newEffect(25, e => {
+const scalarFx = newEffect(45, e => {
   Draw.color(colors.scalar);
   Angles.randLenVectors(e.id, 1, 7+5*e.fin(), floatc2((x,y) => {
     Fill.circle(e.x+x, e.y+y, e.fout()*0.9);
   }));
 });
-const vectorFx = newEffect(25, e => {
+const vectorFx = newEffect(45, e => {
   Draw.color(colors.vector);
   Angles.randLenVectors(e.id, 1, 7+5*e.fin(), floatc2((x,y) => {
     Drawf.tri(e.x+x, e.y+y, e.fout()*2.7, e.fout()*2.7, e.x%5*36*e.fin());
   }));
 });
-const zetaFx = newEffect(25, e => {
+const zetaFx = newEffect(45, e => {
   Draw.color(colors.zeta);
   Lines.stroke(e.fout());
   Angles.randLenVectors(e.id, 1, 7+5*e.fin(), floatc2((x,y) => {
     Lines.poly(e.x+x, e.y+y, 4, e.fin()*0.6+0.3);
   }));
 });
-const codeFx = newEffect(25, e => {
+const codeFx = newEffect(45, e => {
   Draw.color(colors.code);
   Angles.randLenVectors(e.id, 1, 7+5*e.fin(), floatc2((x,y) => {
     drawBit(e.id%2, e.x+x, e.y+y, 1, e.fout());
   }));
 });
 
+/*
 const deposcalar = extendContent(OreBlock, "depo-scalar", {
-  draw(tile){
-    if(!Vars.state.isPaused()) Effects.effect(scalarFx, tile.worldx(), tile.worldy());
-    this.super$draw(tile);
-  }
 });
 //deposcalar.updateEffect = scalarFx;
 //deposcalar.walkEffect = scalarFx;
 const depovector = extendContent(OreBlock, "depo-vector", {
-  draw(tile){
-    if(!Vars.state.isPaused()&&Mathf.chance(0.1)) Effects.effect(vectorFx, tile.worldx(), tile.worldy());
-    this.super$draw(tile);
-  }
 });
 //depovector.updateEffect = vectorFx;
 //depovector.walkEffect = vectorFx;
 const depozeta = extendContent(OreBlock, "depo-zeta", {
-  draw(tile){
-    if(!Vars.state.isPaused()&&Mathf.chance(0.1)) Effects.effect(zetaFx, tile.worldx(), tile.worldy());
-    this.super$draw(tile);
-  }
 });
 //depozeta.updateEffect = zetaFx;
 //depozeta.walkEffect = zetaFx;
 const depocode = extendContent(OreBlock, "depo-code", {
-  draw(tile){
-    if(!Vars.state.isPaused()&&Mathf.chance(0.1)) Effects.effect(codeFx, tile.worldx(), tile.worldy());
-    this.super$draw(tile);
-  }
 });
 //depocode.updateEffect = codeFx;
 //depocode.walkEffect = codeFx;
+*/
+const sparkling = ["commandblocks-depo-scalar", "commandblocks-depo-vector", "commandblocks-depo-zeta", "commandblocks-depo-code"];
+const sparkleFx = [scalarFx, vectorFx, zetaFx, codeFx];
+
+var v1 = new Vec2(0,0);
+Events.on(EventType.Trigger.update, run(function(){
+	v1 = Core.camera.unproject(Mathf.random()*Core.camera.width, Mathf.random()*Core.camera.height);
+  var tile = Vars.world.tileWorld(v1.x, v1.y);
+  if(sparkling.indexOf(tile.overlay().name)>-1) Effects.effect(sparkleFx[sparkling.indexOf(tile.overlay().name)], tile.x, tile.y);
+}));
