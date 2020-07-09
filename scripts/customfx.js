39634c205e802a824e2e4952a21bf2869dd842f7
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-07-09 16:46:00 +0900

    Update customfx.js

--- scripts/customfx.js
+++ scripts/customfx.js
@@ -227,7 +227,7 @@ this.global.fx = {
       Fill.circle(e.x+v1.x, e.y+v1.y, c1);
     }
   }),
-  smokeRise : newEffect(150, e => {
+  smokeRise : newEffectSize(150, 150, e => {
     Draw.color(Color.gray, Pal.darkishGray.cpy().a(0), e.fin());
     var size = 7 + e.fin()*8;
     Draw.rect("circle", e.x+e.fin()*26, e.y+e.fin()*30, size, size);
