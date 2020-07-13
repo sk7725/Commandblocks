0e48c564527009323aa21dc2fe8452789355ece6
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-07-13 20:55:48 +0900

    Update customb.js

--- scripts/customb.js
+++ scripts/customb.js
@@ -539,8 +539,9 @@ const flashbang = extend(BasicBulletType,{
     if(x === undefined || x === null){
       x = b.x; y = b.y;
     }
-    const v1 = Core.camera.unproject(0, 0);
-    const v2 = Core.camera.unproject(Core.graphics.getWidth(), Core.graphics.getHeight());
+    const v2 = Core.camera.unproject(0, 0);
+    const v1 = Core.camera.unproject(Core.graphics.getWidth(), Core.graphics.getHeight());
+    print(v1); print(v2); print("Pos: ("+x+", "+y+")");
     if(v1.x<x && x<v2.x && v1.y<y && y<v2.y) this.flash((b.getTeam()==Vars.player.getTeam())?4:11);
     
     this.super$hit(b, x, y);
@@ -562,7 +563,7 @@ const flashbang = extend(BasicBulletType,{
   //despawned(b){},
   update(b){
     this.super$update(b);
-    if(Mathf.chance(0.2)) Effects.effect(flashSpark, b.x, b.y);
+    if(Mathf.chance(0.2)) Effects.effect(flashSpark, b.x, b.y - b.fin()*(b.fin()-1)*190);
   }
 });
 flashbang.speed = 3.2;
