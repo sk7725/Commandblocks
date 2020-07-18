6396d8b132c4e698f68951341b98f4a6c236f0fa
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-07-18 20:15:31 +0900

    Create sporeblock.js

---
+++ scripts/sporeblock.js
@@ -0,0 +1,41 @@
+const modName = "commandblocks-";
+const slimeBlocks = [modName+"sporeblock", modName+"tensorbind", modName+"sporedir", modName+"tensordir"];
+const sporeblock = extendContent(Block, "sporeblock",{
+  canReplace(other){
+    return slimeBlocks.indexOf(other.name) > -1;
+  }
+});
+
+const tensorbind = extendContent(Block, "tensorbind",{
+  canReplace(other){
+    return slimeBlocks.indexOf(other.name) > -1;
+  }
+});
+
+const sporedir = extendContent(Block, "sporedir",{
+  canReplace(other){
+    return slimeBlocks.indexOf(other.name) > -1;
+  },
+  load(){
+    this.super$load();
+    this.dirRegion = [];
+    for(var i=0;i<4;i++) this.dirRegion.push(Core.atlas.find(this.name+"-"+i));
+  },
+  draw(tile){
+    Draw.rect(this.dirRegion[tile.rotation()], tile.drawx(), tile.drawy());
+  }
+});
+
+const tensordir = extendContent(Block, "tensordir",{
+  canReplace(other){
+    return slimeBlocks.indexOf(other.name) > -1;
+  },
+  load(){
+    this.super$load();
+    this.dirRegion = [];
+    for(var i=0;i<4;i++) this.dirRegion.push(Core.atlas.find(this.name+"-"+i));
+  },
+  draw(tile){
+    Draw.rect(this.dirRegion[tile.rotation()], tile.drawx(), tile.drawy());
+  }
+});
