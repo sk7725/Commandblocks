8363343dffe1081182aad91173c13e161fe9a833
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-07-21 12:26:05 +0900

    warning

--- scripts/init.js
+++ scripts/init.js
@@ -8,3 +8,12 @@ StatusEffects.shocked.color = Pal.lancerLaser;
 StatusEffects.corroded.color = Pal.plastanium;
 StatusEffects.boss.color = Pal.health;
 StatusEffects.shielded.color = Pal.stoneGray;
+
+if (Vars.ui.hudGroup){
+
+  Core.app.post(run(() => {
+    Vars.ui.showCustomConfirm(Core.bundle.get("warning.title"), Core.bundle.get("warning.text"), "[accent]"+Core.bundle.get("ok")+"[]", "[lightgray]"+Core.bundle.get("cancel")+"[]", run(()=>{
+      Core.app.exit();
+    }), run(()=>{}));
+  }));
+}
