37a7d0a8bb6dafb1d34783a86547d82e507d3704
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-07-24 18:06:54 +0900

    as

--- scripts/walls/wallinvi.js
+++ scripts/walls/wallinvi.js
@@ -15,14 +15,19 @@ const bigx = newEffect(20, e => {
 });
 
 const wallinvi = extendContent(Wall,"wallinvi",{
-    handleDamage(tile,amount){
-      if(amount > 2) Effects.effect(smallx, Pal.heal, tile.drawx(), tile.drawy());
-      return 0.0;
-    },
-    handleBulletHit(entity,bullet){
-      entity.damage(0.0);
-      Effects.effect(smallx, colorx, entity.getX(), entity.getY());
-    }
+  handleDamage(tile,amount){
+    if(amount > 2) Effects.effect(smallx, Pal.heal, tile.drawx(), tile.drawy());
+    return 0.0;
+  },
+  handleBulletHit(entity,bullet){
+    entity.damage(0.0);
+    Effects.effect(smallx, colorx, entity.getX(), entity.getY());
+  },
+  setStats(){
+    this.super$setStats();
+    this.stats.remove(BlockStat.health);
+    this.stats.add(BlockStat.health, "{0}", Core.bundle.get("stat.infinity"));
+  }
 });
 
 wallinvi.entityType = prov(() => extend(TileEntity , {
@@ -33,14 +38,19 @@ wallinvi.entityType = prov(() => extend(TileEntity , {
 }));
 
 const wallinvilarge = extendContent(Wall,"wallinvilarge",{
-    handleDamage(tile,amount){
-      if(amount > 2) Effects.effect(bigx, Pal.heal, tile.drawx(), tile.drawy());
-      return 0.0;
-    },
-    handleBulletHit(entity,bullet){
-      entity.damage(0.0);
-      Effects.effect(bigx, colorx, entity.getX(), entity.getY());
-    }
+  handleDamage(tile,amount){
+    if(amount > 2) Effects.effect(bigx, Pal.heal, tile.drawx(), tile.drawy());
+    return 0.0;
+  },
+  handleBulletHit(entity,bullet){
+    entity.damage(0.0);
+    Effects.effect(bigx, colorx, entity.getX(), entity.getY());
+  },
+  setStats(){
+    this.super$setStats();
+    this.stats.remove(BlockStat.health);
+    this.stats.add(BlockStat.health, "{0}", Core.bundle.get("stat.infinity"));
+  }
 });
 
 wallinvilarge.entityType = prov(() => extend(TileEntity , {
