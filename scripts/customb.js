048b56aeb82098400ca9ae92a2ccb4d89fe0f23a
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-07-26 21:00:13 +0900

    Update customb.js

--- scripts/customb.js
+++ scripts/customb.js
@@ -864,7 +864,7 @@ this.global.bullets.spear2 = spear2;
 const ball = extend(BasicBulletType,{
   draw(b){
     Draw.color();
-    Draw.rect(this.backRegion, b.x, b.y, Time.time()*(b.id%4+1)*1.5);
+    Draw.rect(this.backRegion, b.x, b.y, Time.time()*(b.id%4+1)*4);
     Draw.rect(this.frontRegion, b.x, b.y);
   }
   //hit(b,x,y){},
@@ -874,7 +874,7 @@ const ball = extend(BasicBulletType,{
 
 ball.speed = 2.5;
 ball.knockback = 40;
-ball.lifetime = 130;
+ball.lifetime = 100;
 ball.pierce = true;
 ball.damage = 0;
 ball.collidesTiles = true;
