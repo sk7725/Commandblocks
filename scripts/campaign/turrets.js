d9642ce553900cab576de1bfe4f74ba4196385ea
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-07-28 22:45:55 +0900

    Update turrets.js

--- scripts/campaign/turrets.js
+++ scripts/campaign/turrets.js
@@ -1,4 +1,15 @@
+const undyneShoot = newEffect(25, e => {
+Draw.color(Pal.lancerLaser, Color.white, e.fout());
+  Lines.stroke(e.fout());
+  var v1 = Vec2(e.fin()*4,0).setAngle(e.rotation+180);
+  Lines.circle(e.x+v1.x, e.y+v1.y, e.fin()*25);
+  Lines.stroke(e.fout()*1.5);
+  Draw.color(Color.white);
+  Angles.randLenVectors(e.id, 6, 20*e.finpow(), e.rotation, 50, floatc2((x,y) => {
+    Lines.lineAngle(e.x+x, e.y+y, Mathf.angle(x, y), 1+e.fout()*4);
+  }));
+});
 const undyne = extendContent(PowerTurret, "undyne", {});
 
-undyne.shootEffect = Fx.hitFuse;
+undyne.shootEffect = undyneShoot;
 undyne.shootType = this.global.bullets.spear;
