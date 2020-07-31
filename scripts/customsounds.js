23c40150cad471f66f1a6b5dd912676992f343a3
younggam <61054554+younggam@users.noreply.github.com>
2020-07-31 22:11:43 +0900

    Update customsounds.js

--- scripts/customsounds.js
+++ scripts/customsounds.js
@@ -1,11 +1,11 @@
 if(!this.global.newSounds){
   this.global.newSounds = {};
 }
-
+importPackage(Packages.arc.audio);
 var t = this;
 
 function loadsound(name){
-  if(t.global.newSounds[name] !== undefined && t.global.newSounds[name] !== null && t.global.newSounds[name] != Sounds.none) return;
+  /*if(t.global.newSounds[name] !== undefined && t.global.newSounds[name] !== null && t.global.newSounds[name] != Sounds.none) return;
   try{
     (Core.assets.load("sounds/"+ name +".ogg", Packages.arc.audio.Sound)).loaded = cons(a => {
       try{
@@ -27,7 +27,10 @@ function loadsound(name){
   if(!t.global.newSounds[name]){
     t.global.newSounds[name] = Sounds.none;
     //print("Failed to load sound! Please restart the game!");
-  }
+  }*/
+  var path="sounds/"+name+".ogg";
+  if(Core.assets.contains(path,Sound)) t.global.newSounds[name]=Core.assets.get(path,Sound);
+  else Core.assets.load(path,Sound).loaded=cons(a=>t.gloabl.newSounds[name]=a);
 }
 
 loadsound("boostsound");
