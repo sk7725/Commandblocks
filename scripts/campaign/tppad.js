30ac3d0ad5e09b03fb57f6ec0003dc3b0b146619
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-08-16 16:28:48 +0900

    tp pad

--- scripts/campaign/tppad.js
+++ scripts/campaign/tppad.js
@@ -1,24 +1,51 @@
 
 const tppad = extendContent(Block, "tppad", {
   tapped(tile, player){
-    const entity = tile.ent();
-    const other = Vars.world.tile(entity.link);
-    if(this.linkValid(tile, other) && entity.power.status > 0.5 && !player.isDead() && other != null && other.entity != null && entity.getchargeProgress() <= 0){
-      if(player.dst(tile) > this.size * Vars.tilesize / 2 + 12){
-        player.moveTarget = player.moveTarget == tile.entity ? null : tile.entity;
-      }
-      else{
-        entity.setchargeProgress(Mathf.dst(tile.drawx(), tile.drawy(),other.drawx(), other.drawy()) / 3);
-        entity.setconAble(1);
-        player.set(other.drawx(),other.drawy());
-        if(player == Vars.player) Core.camera.position.set(player);
-        Time.run(2 , run(() => {
-          Sounds.respawn.at(other);
-          Sounds.respawn.at(tile);
-          Effects.effect(Fx.spawn, other);
-          Effects.effect(Fx.spawn, tile);
-        }))
-    	}
+    if(this.checkValidTap(tile, player)){
+      tile.configure(1);
+    }else if(player.isLocal && Vars.mobile && !player.isDead() && tile.ent().cons.valid()){
+      //deselect on double taps
+      player.moveTarget = (player.moveTarget == tile.ent()) ? null : tile.ent();
     }
+  },
+  configured(tile, player, value){
+    if(value != 1) return;
+    if(player == null || tile == null || tile.block()!=this || !checkValidTap(tile, player)) return;
+    if(!tile.ent().cons.valid()) return;
+    this.tpPlayer(tile, player);
+  },
+  checkValidTap(tile, player){
+    return !player.isDead() && tile.interactable(player.getTeam()) && Math.abs(player.x - tile.drawx()) <= tile.block().size * Vars.tilesize && Math.abs(player.y - tile.drawy()) <= tile.block().size * Vars.tilesize && tile.ent().cons.valid();
+  },
+  tpPlayer(tile, player){
+    var arr = stile.ent().power.links.toArray();//list of pos
+    arr = arr.filter(pos => Vars.world.tile(pos).block().name == "commandblocks-tppad");
+    arr.sort(function(a, b) {
+      return a - b;
+    });
+    var index = arr.indexOf(tile.pos());
+    if(index < 0){
+      print("Err: parent pad not in link!");
+      print(arr);
+      return;
+    }
+    index++;
+    if(index >= arr.length) index = 0;
+    var etile = Vars.world.tile(arr[index]);
+    player.set(etile.drawx(), etile.drawy());
+    if(player == Vars.player) Core.camera.position.set(player);
+    Effects.effect(Fx.teleportActivate, Pal.lancerLaser, etile.drawx(), etile.drawy());
+    Effects.effect(Fx.teleportOut, Pal.lancerLaser, tile.drawx(), tile.drawy());
+  },
+  draw(tile){
+    this.super$draw(tile);
+    if(!tile.ent().cons.valid()) return;
+    Draw.color(Pal.lancerLaser, Color.white, Mathf.sin(Time.time()*0.01));
+    Draw.alpha(Mathf.sin(Time.time()*0.05)*0.5+0.5);
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+  },
+  load(){
+    this.super$load();
+    this.topRegion = Core.atlas.find(this.name + "-top");
   }
 }
