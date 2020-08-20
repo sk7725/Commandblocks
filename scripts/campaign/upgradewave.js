f4976a455b0057428094a5444124f99618724af8
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-08-20 20:35:14 +0900

    plz

--- scripts/campaign/upgradewave.js
+++ scripts/campaign/upgradewave.js
@@ -1,4 +1,5 @@
 const customb = this.global.bullets;
+const newSounds = this.global.newSounds;
 const shader = this.global.shaders.bittrium;
 
 function copyWeapon(orig, target){
@@ -10,14 +11,14 @@ function copyWeapon(orig, target){
       target[arr[i]] = orig[arr[i]];
     }
     catch(err){
-      print("CopyWeapon Err: "+err);
+      //print("CopyWeapon Err: "+err);
     }
   }
   return target;
 }
 
 function copyUnitType(orig, target){
-  const blacklist = ["UnitType", "weapon", "create", "displayInfo", "load", "getContentType", "name", "localizedName", "description"];
+  const blacklist = ["UnitType", "weapon", "create", "displayInfo", "load", "getContentType", "name", "localizedName", "description", "constructor", "typeID"];
   blacklist = blacklist.concat(Object.keys(UnlockableContent));
   var arr = Object.keys(orig);
   for(var i=0; i<arr.length; i++){
@@ -26,7 +27,7 @@ function copyUnitType(orig, target){
       target[arr[i]] = orig[arr[i]];
     }
     catch(err){
-      print("CopyUnitType Err: "+err);
+      //print("CopyUnitType Err: "+err);
     }
   }
   return target;
@@ -45,6 +46,34 @@ function createUnit(name, cbullet, type){
       this.region = Core.atlas.find(name);
       this.baseRegion = Core.atlas.find(name + "-base");
       this.legRegion = Core.atlas.find(name + "-leg");
+    },
+    displayInfo(table){
+      table.table(cons(title => {
+        title.addImage(origtype.icon(Cicon.xlarge)).size(8 * 6);
+        title.add("[pink]" + this.localizedName).padLeft(5);
+      }));
+
+      table.row();
+
+      table.addImage().height(3).color(Color.lightGray).pad(15).padLeft(0).padRight(0).fillX();
+
+      table.row();
+
+      if(this.description != null){
+        table.add(this.displayDescription()).padLeft(5).padRight(5).width(400f).wrap().fillX();
+        table.row();
+
+        table.addImage().height(3).color(Color.lightGray).pad(15).padLeft(0).padRight(0).fillX();
+        table.row();
+      }
+
+      table.left().defaults().fillX();
+
+      table.add(Core.bundle.format("unit.health", this.health));
+      table.row();
+      table.add(Core.bundle.format("unit.speed", this.speed.toFixed(1)));
+      table.row();
+      table.row();
     }
   });
   unittype = copyUnitType(origtype, unittype);
@@ -84,9 +113,22 @@ function createUnit(name, cbullet, type){
 
 const dagger2 = createUnit("dagger", "lightning", GroundUnit);
 dagger2.weapon.shootSound = Sounds.spark;
-dagger2.health = 300;
+dagger2.health = 700;
+
+const titan2 = createUnit("titan", "spear", GroundUnit);
+titan2.weapon.shootSound = Sounds.none;
+titan2.health = 2000;
 
 const fortress2 = createUnit("fortress", "lancerLaser", GroundUnit);
-fortress2.health = 1200;
+fortress2.weapon.shootSound = Sounds.laser;
+fortress2.weapon.reload = 15;
+fortress2.health = 6400;
 fortress2.speed = 0.17;
 fortress2.maxVelocity = 1.2;
+
+const fortress2 = createUnit("wraith", "grenade", FlyingUnit);
+wraith2.weapon.shootSound = Sounds.artillery;
+wraith2.weapon.reload = 45;
+wraith2.health = 560;
+wraith2.speed = 0.33;
+wraith2.maxVelocity = 2.5;
