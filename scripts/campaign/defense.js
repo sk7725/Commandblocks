4b49fa9649a23904d7ef6f19ee31f09ba2162067
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-08-27 14:04:54 +0900

    heck

--- scripts/campaign/defense.js
+++ scripts/campaign/defense.js
@@ -1,27 +1,50 @@
 const mendDome = extendContent(MendProjector, "mend-dome", {});
 
-const overDome = extendContent(MendProjector, "overdrive-dome", {
+const overDome = extendContent(OverdriveProjector, "overdrive-dome", {
   draw(tile){
     Draw.rect(this.region, tile.drawx(), tile.drawy());
 
     var entity = tile.ent();
     var f = 1 - (Time.time() / 100) % 1;
 
-    if(entity.phaseHeat == undefined) entity.phaseHeat = 0;//wtf
-    Draw.color(this.baseColor, this.phaseColor, entity.phaseHeat);
-    Draw.alpha(entity.heat * Mathf.absin(Time.time(), 10, 1) * 0.5);
+    Draw.color(this.baseColor, this.phaseColor, entity.getPhaseHeat());
+    Draw.alpha(entity.getHeat() * Mathf.absin(Time.time(), 10, 1) * 0.5);
     //Draw.blend(Blending.additive);
     Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
     //Draw.blend();
 
     Draw.alpha(1);
-    Lines.stroke((2 * f + 0.2) * entity.heat);
+    Lines.stroke((2 * f + 0.2) * entity.getHeat());
     Lines.square(tile.drawx(), tile.drawy(), ((1 - f) * 8) * this.size / 2);
 
     Draw.reset();
+  },
+  update(tile){
+    this.super$update(tile);
+    var entity = tile.ent();
+    entity.setHeat(Mathf.lerpDelta(entity.getHeat(), (entity.cons.valid() || tile.isEnemyCheat())? 1 : 0, 0.08));
+    entity.setPhaseHeat(Mathf.lerpDelta(entity.getPhaseHeat(), Mathf.num(entity.cons.optionalValid()), 0.1));
   }
 });
 
+overDome.entityType = prov(() => extend(OverdriveProjector.OverdriveEntity, {
+  _heat: 0,
+  _phase: 0,
+  getHeat(){
+    return this._heat;
+  },
+  setHeat(a){
+    this._heat = a;
+  },
+  getPhaseHeat(){
+    return this._phase;
+  },
+  setPhaseHeat(a){
+    this._phase = a;
+  }
+}));
+
+/*
 const rageZone = this.global.bullets.rageZone;
 const rageDome = extendContent(MendProjector, "rage-dome", {
   setStats(){
@@ -30,11 +53,11 @@ const rageDome = extendContent(MendProjector, "rage-dome", {
   },
   update(tile){
     var entity = tile.ent();
-    if(entity.phaseHeat == undefined) entity.phaseHeat = 0;//wtf
+    //if(entity.phaseHeat == undefined) entity.phaseHeat = 0;//wtf
     entity.heat = Mathf.lerpDelta(entity.heat, (entity.cons.valid() || tile.isEnemyCheat())? 1 : 0, 0.08);
     entity.charge += entity.heat * entity.delta();
 
-    entity.phaseHeat = Mathf.lerpDelta(entity.phaseHeat, Mathf.num(entity.cons.optionalValid()), 0.1);
+    //entity.phaseHeat = Mathf.lerpDelta(entity.phaseHeat, Mathf.num(entity.cons.optionalValid()), 0.1);
 
     if(entity.cons.optionalValid() && entity.timer.get(this.timerUse, this.useTime) && entity.efficiency() > 0){
       entity.cons.trigger();
@@ -46,3 +69,4 @@ const rageDome = extendContent(MendProjector, "rage-dome", {
     }
   }
 });
+*/
