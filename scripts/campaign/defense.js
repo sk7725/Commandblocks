336799d65fb991429143c032153f5cb43ba5d200
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-08-27 14:13:19 +0900

    the heck

--- scripts/campaign/defense.js
+++ scripts/campaign/defense.js
@@ -1,32 +1,40 @@
 const mendDome = extendContent(MendProjector, "mend-dome", {});
 
 const overDome = extendContent(OverdriveProjector, "overdrive-dome", {
+  _heat: [],
+  _phase: [],
   draw(tile){
     Draw.rect(this.region, tile.drawx(), tile.drawy());
 
     var entity = tile.ent();
     var f = 1 - (Time.time() / 100) % 1;
 
-    Draw.color(this.baseColor, this.phaseColor, entity.getPhaseHeat());
-    Draw.alpha(entity.getHeat() * Mathf.absin(Time.time(), 10, 1) * 0.5);
+    Draw.color(this.baseColor, this.phaseColor, this._phase[tile.pos()]);
+    Draw.alpha(this._heat[tile.pos()] * Mathf.absin(Time.time(), 10, 1) * 0.5);
     //Draw.blend(Blending.additive);
     Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
     //Draw.blend();
 
     Draw.alpha(1);
-    Lines.stroke((2 * f + 0.2) * entity.getHeat());
+    Lines.stroke((2 * f + 0.2) * this._heat[tile.pos()]);
     Lines.square(tile.drawx(), tile.drawy(), ((1 - f) * 8) * this.size / 2);
 
     Draw.reset();
   },
+  placed(tile){
+    this.super$placed(tile);
+    this._heat[tile.pos()] = 0;
+    this._phase[tile.pos()] = 0;
+  }
   update(tile){
     this.super$update(tile);
-    var entity = tile.ent();
-    entity.setHeat(Mathf.lerpDelta(entity.getHeat(), (entity.cons.valid() || tile.isEnemyCheat())? 1 : 0, 0.08));
-    entity.setPhaseHeat(Mathf.lerpDelta(entity.getPhaseHeat(), Mathf.num(entity.cons.optionalValid()), 0.1));
+    if(this._heat[tile.pos()] == undefined) this._heat[tile.pos()] = 0;
+    if(this._phase[tile.pos()] == undefined) this._phase[tile.pos()] = 0;
+    this._heat[tile.pos()] = Mathf.lerpDelta(this._heat[tile.pos()], (tile.ent().cons.valid() || tile.isEnemyCheat())? 1 : 0, 0.08);
+    this._phase[tile.pos()] = Mathf.lerpDelta(this._phase[tile.pos()], Mathf.num(tile.ent().cons.optionalValid()), 0.1);
   }
 });
-
+/*
 overDome.entityType = prov(() => extend(OverdriveProjector.OverdriveEntity, {
   _heat: 0,
   _phase: 0,
@@ -42,7 +50,7 @@ overDome.entityType = prov(() => extend(OverdriveProjector.OverdriveEntity, {
   setPhaseHeat(a){
     this._phase = a;
   }
-}));
+}));*/
 
 /*
 const rageZone = this.global.bullets.rageZone;
