b35eb1e561bff26ab69be2b9c91727443800d013
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-12-24 11:57:17 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,12 @@
-if(!Vars.headless && Vars.mods.locateMod("betamindy") == null){
+if(!Vars.headless){
   Events.on(ClientLoadEvent, () => {
     Core.app.post(() => {
-      Vars.ui.showConfirm("$download.title", "$download.text", () => {
-        Core.app.openURI("https://github.com/sk7725/BetaMindy");
-      });
+      if(Vars.mods.locateMod("betamindy") == null){
+        Vars.ui.showConfirm("$download.title", "$download.text", () => {
+          Core.app.openURI("https://github.com/sk7725/BetaMindy");
+        });
+      }
+
       var mod = Vars.mods.locateMod("beta-mindy-r");
       mod.meta.displayName = "[gray]Beta[lightgray]Mindy[][]";
       mod.meta.description = "$download.text";
